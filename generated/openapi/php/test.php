6bf4da916376e142e1427cbc9f6c3f9719bf2e3d
Damien Couchez <damien.couchez@gmail.com>
2021-10-29 14:28:41 +0200

    add some custom files and update test

--- generated/openapi/php/test.php
+++ generated/openapi/php/test.php
@@ -1,24 +1,34 @@
 <?php
 
 require 'vendor/autoload.php';
-use OpenAPI\Client\Api\DefaultApi;
-use OpenAPI\Client\Model\InlineObject;
-use OpenAPI\Client\Configuration;
 
-$config = new Configuration();
+use NewApiClient\Client\RecommendClient as NewRecommendClient;
+use NewApiClient\Client\Config\RecommendConfig;
+use OpenAPI\Client\Model\InlineObject;
+
+$config = new RecommendConfig();
+
 $appId = getenv('ALGOLIA_APPLICATION_ID_1');
 $apiKey = getenv('ALGOLIA_ADMIN_KEY_1');
-$config->setHost("https://{$appId}-1.algolianet.com");
 
-$client = new DefaultApi(null, $config);
+$config->setHost("https://{$appId}-1.algolianet.com");
+$config->setAlgoliaApiKey($apiKey);
+$config->setAppId($appId);
+
+$client = new NewRecommendClient(null, $config);
 
 $object = new InlineObject([
-    'index_name' => 'products',
+    'index_name' => 'recommend_test',
     'object_id' => 'B018APC4LE',
     'model' => 'bought-together',
 ]);
 
-echo $client->getRecommendations([$object], $appId, $apiKey);
+try {
+    $client->getRecommendations([$object]);
+} catch(\OpenAPI\Client\ApiException $e) {
+    echo "API contacted!\n";
+}
+
 
 // Open API
 //"[{"indexName":"products","model":"bought-together","objectID":"B018APC4LE","threshold":0,"maxRecommendations":0}]"
