6162a4ae48d8aa61abe126cbcaffb081d6518b31
Pierre Millot <pierre.millot@algolia.com>
2021-11-26 16:28:51 +0100

    add optional status

--- clients/utils/javascript/requester/EchoRequester.ts
+++ clients/utils/javascript/requester/EchoRequester.ts
@@ -2,11 +2,15 @@ import { EndRequest, Request, Response } from '../types';
 import { Requester } from './Requester';
 
 export class EchoRequester extends Requester {
+  constructor(private status = 200) {
+    super();
+  }
+
   async send(request: EndRequest, originalRequest: Request): Promise<Response> {
     return {
       content: JSON.stringify(originalRequest),
       isTimedOut: false,
-      status: 200,
+      status: this.status,
     };
   }
 }
--- playground/javascript/app.ts
+++ playground/javascript/app.ts
@@ -1,4 +1,4 @@
-import { searchClient, ApiError, EchoRequester } from 'algoliasearch-client-javascript';
+import { searchClient, ApiError } from 'algoliasearch-client-javascript';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
@@ -10,7 +10,7 @@ const searchIndex = process.env.SEARCH_INDEX || 'test_index';
 const searchQuery = process.env.SEARCH_QUERY || 'test_query';
 
 // Init client with appId and apiKey
-const client = new searchClient(appId, apiKey, { requester: new EchoRequester() });
+const client = new searchClient(appId, apiKey);
 
 async function testMultiQueries() {
   try {
