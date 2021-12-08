e45bd380a6f1492e5014aabf5066387a6ef2b1a4
Pierre Millot <pierre.millot@algolia.com>
2021-12-08 17:05:47 +0100

    format and fix oneOf

--- scripts/post-gen-java.sh
+++ scripts/post-gen-java.sh
@@ -1,3 +1,7 @@
 CLIENT_FOLDER=$1
 
 find $CLIENT_FOLDER -type f -name "*.java" | xargs sed -i '' -e 's/= {}/= new Object()/g'
+
+echo "package com.algolia.model;public class OneOfintegerstring {}" > $CLIENT_FOLDER/algoliasearch-core/com/algolia/model/OneOfintegerstring.java
+
+prettier --write $CLIENT_FOLDER/**/*.java
