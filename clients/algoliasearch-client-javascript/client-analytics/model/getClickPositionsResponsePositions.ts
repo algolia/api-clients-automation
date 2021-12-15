38385de345fb02c9f304061b2492b35e4187969d
Clément Vannicatte <vannicattec@gmail.com>
2021-12-15 10:32:54 +0100

    apply changes from suggestion

--- clients/algoliasearch-client-javascript/client-analytics/model/getClickPositionsResponsePositions.ts
+++ clients/algoliasearch-client-javascript/client-analytics/model/getClickPositionsResponsePositions.ts
@@ -2,9 +2,9 @@ export type GetClickPositionsResponsePositions = {
   /**
    * Range of positions with the following pattern:  Positions from 1 to 10 included are displayed in separated groups. Positions from 11 to 20 included are grouped together. Positions from 21 and up are grouped together.
    */
-  position?: number[];
+  position: number[];
   /**
    * The number of click event.
    */
-  clickCount?: number;
+  clickCount: number;
 };
