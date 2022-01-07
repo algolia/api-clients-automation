965d2ba0253d56d9bb93badde5ae6ef42c8bea3e
Pierre Millot <pierre.millot@algolia.com>
2022-01-07 18:08:35 +0100

    fix spec

--- clients/algoliasearch-client-javascript/client-search/model/operation.ts
+++ clients/algoliasearch-client-javascript/client-search/model/operation.ts
@@ -2,10 +2,7 @@ import type { Action } from './action';
 
 export type Operation = {
   action?: Action;
-  /**
-   * Arguments to the operation (depends on the type of the operation).
-   */
-  body?: { [key: string]: Record<string, any> };
+  body?: Record<string, any>;
   /**
    * Index to target for this operation.
    */
