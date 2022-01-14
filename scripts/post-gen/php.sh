931e750d4e8deab82ac4af7d74f76b4a5c5a2561
Damien Couchez <damien.couchez@gmail.com>
2022-01-14 16:31:35 +0100

    fix: update post generation script

--- scripts/post-gen/php.sh
+++ scripts/post-gen/php.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
+set -e
 export GENERATOR=$1
 export CLIENT=$(cat openapitools.json | jq -r --arg generator "$GENERATOR" '."generator-cli".generators[$generator].output' | sed 's/#{cwd}\///g')
 
 lint_client() {
-    set +e
 
     echo "> Linting ${GENERATOR}..."
     cd $CLIENT
     composer install
     PHP_CS_FIXER_IGNORE_ENV=1 php vendor/bin/php-cs-fixer fix lib/ -v --using-cache=no --allow-risky=yes
 
-    set -e
 }
 
 lint_client
