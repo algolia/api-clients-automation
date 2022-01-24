a7c9514d95e037273e9df8d898655780dc58d9dc
Eunjae Lee <karis612@gmail.com>
2022-01-24 11:49:44 +0100

    chore: format cts output

--- tests/output/javascript/tests/client/analytics.test.ts
+++ tests/output/javascript/tests/client/analytics.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable require-await */
+
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 // @ts-nocheck
 import { AnalyticsApi, EchoRequester } from '@algolia/client-analytics';
@@ -8,53 +8,48 @@ const appId = process.env.ALGOLIA_APPLICATION_ID || 'Algolia-API-Key';
 const apiKey = process.env.ALGOLIA_SEARCH_KEY || 'Algolia-Application-Id';
 
 function createClient() {
-    return new AnalyticsApi(appId, apiKey, 'us', { requester: new EchoRequester() });
+  return new AnalyticsApi(appId, apiKey, 'us', {
+    requester: new EchoRequester(),
+  });
 }
 
 async function createIndex() {}
 
 describe('basic', () => {
-    test('does not throw when region is not given', async () => {
+  test('does not throw when region is not given', async () => {
+    let actual;
 
-        let actual;
+    await expect(
+      new Promise((resolve, reject) => {
+        const $client = new AnalyticsApi('my-app-id', 'my-api-key', '', {
+          requester: new EchoRequester(),
+        });
+        actual = $client;
 
-                    await expect(new Promise((resolve, reject) => {
-                            const $client = new AnalyticsApi(
-                                              'my-app-id',
-                                              'my-api-key',
-                                              '',
-                                              {
-                                                  requester: new EchoRequester()
-                                              }
-                                            )
-                            actual = $client;
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).resolves.not.toThrow();
+  });
 
+  test('getAverageClickPosition throws without index', async () => {
+    const $client = createClient();
 
-                        if (actual instanceof Promise) {
-                            actual.then(resolve).catch(reject);
-                        } else {
-                            resolve();
-                        }
-                    })).resolves.not.toThrow();
-                
-    });
-
-    test('getAverageClickPosition throws without index', async () => {
-            const $client = createClient();
-
-        let actual;
-                await expect(new Promise((resolve, reject) => {
-
-
-                        actual = $client.getClickPositions({});
-                    if (actual instanceof Promise) {
-                        actual.then(resolve).catch(reject);
-                    } else {
-                        resolve();
-                    }
-                })).rejects.toThrow("Parameter `index` is required when calling `getClickPositions`.")
-
-    });
-
-})
-
+    let actual;
+    await expect(
+      new Promise((resolve, reject) => {
+        actual = $client.getClickPositions({});
+        if (actual instanceof Promise) {
+          actual.then(resolve).catch(reject);
+        } else {
+          resolve();
+        }
+      })
+    ).rejects.toThrow(
+      'Parameter `index` is required when calling `getClickPositions`.'
+    );
+  });
+});
