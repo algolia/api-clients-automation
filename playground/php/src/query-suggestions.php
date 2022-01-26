94eecddd0d6ec9f2c054ee576a49bfad45cda36d
Damien Couchez <damien.couchez@gmail.com>
2022-01-26 17:38:50 +0100

    feat: add playgrounds

---
+++ playground/php/src/query-suggestions.php
@@ -0,0 +1,9 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Algolia\AlgoliaSearch\Api\QuerySuggestionsApi;
+
+$client = QuerySuggestionsApi::create(getenv('ALGOLIA_APPLICATION_ID'), getenv('QUERY_SUGGESTIONS_KEY'));
+
+var_dump($client->getAllConfigs());
