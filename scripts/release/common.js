6a34b36e31c3610dc4ff080564b51a03825221bc
Eunjae Lee <karis612@gmail.com>
2022-02-11 16:50:42 +0100

    chore: clean up common

--- scripts/release/common.js
+++ scripts/release/common.js
@@ -2,6 +2,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/no-commonjs */
 const execa = require('execa'); // https://github.com/sindresorhus/execa/tree/v5.1.1
+const config = require('../../config.json');
+const openapitools = require('../../openapitools.json');
 
 function run(command, errorMessage = undefined) {
   let result;
@@ -19,11 +21,19 @@ function run(command, errorMessage = undefined) {
   return result.stdout;
 }
 
+const LANGS = [
+  ...new Set(
+    Object.keys(openapitools['generator-cli'].generators).map(
+      (key) => key.split('-')[0]
+    )
+  ),
+];
+
 module.exports = {
-  RELEASED_TAG: 'released',
-  MAIN_BRANCH: 'main',
-  OWNER: 'algolia',
-  REPO: 'api-clients-automation',
-  LANGS: ['javascript', 'php', 'java'],
+  RELEASED_TAG: config.release.releasedTag,
+  MAIN_BRANCH: config.release.mainBranch,
+  OWNER: config.release.owner,
+  REPO: config.release.repo,
+  LANGS,
   run,
 };
