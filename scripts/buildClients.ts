e22626a98623e5ab8a0f66a8ec66f0b940015399
Pierre Millot <pierre.millot@algolia.com>
2022-02-24 16:30:29 +0100

    fix build

--- scripts/buildClients.ts
+++ scripts/buildClients.ts
@@ -1,12 +1,13 @@
 import { run } from './common';
+import { getLanguageFolder } from './config';
 import { createSpinner } from './oraLog';
 import type { Generator } from './types';
 
-async function buildClient(
-  { language, additionalProperties: { packageName } }: Generator,
+async function buildPerClient(
+  { language, key, additionalProperties: { packageName } }: Generator,
   verbose: boolean
 ): Promise<void> {
-  const spinner = createSpinner(`building ${language}`, verbose).start();
+  const spinner = createSpinner(`building ${key}`, verbose).start();
   switch (language) {
     case 'javascript':
       await run(`yarn workspace ${packageName} clean`, { verbose });
@@ -15,9 +16,22 @@ async function buildClient(
         { verbose }
       );
       break;
+    default:
+  }
+  spinner.succeed();
+}
+
+async function buildAllClients(
+  language: string,
+  verbose: boolean
+): Promise<void> {
+  const spinner = createSpinner(`building ${language}`, verbose).start();
+  switch (language) {
     case 'java':
       await run(
-        `./gradle/gradlew --no-daemon -p clients/${packageName} assemble`,
+        `./gradle/gradlew --no-daemon -p ${getLanguageFolder(
+          language
+        )} assemble`,
         {
           verbose,
         }
@@ -35,6 +49,17 @@ export async function buildClients(
   verbose: boolean
 ): Promise<void> {
   for (const gen of generators) {
-    await buildClient(gen, verbose);
+    // we should detect this from the config file, for example `isMutliBuild`
+    if (gen.language === 'javascript') {
+      await buildPerClient(gen, verbose);
+    }
+  }
+
+  // for other languages, we can mutualize the build
+  const langs = [...new Set(generators.map((gen) => gen.language))];
+  for (const lang of langs) {
+    if (lang !== 'javascript') {
+      await buildAllClients(lang, verbose);
+    }
   }
 }
