8f3272a422dba0a6d93604beb3e1c95a16e947b7
Clément Vannicatte <vannicattec@gmail.com>
2022-03-09 18:52:30 +0100

    feat(ci): make ci push generated code

---
+++ scripts/ci/codegen/cleanGeneratedBranch.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+import { run } from '../../common';
+
+import { branchExists } from './common';
+
+/**
+ * Deletes a branch for it's `generated/headRef` name on origin.
+ *
+ * @param headRef - The name of the branch to search for.
+ */
+export async function cleanGeneratedBranch(headRef?: string): Promise<void> {
+  if (!process.env.HEAD_REF && !headRef) {
+    throw new Error('Unable to run cleanup, HEAD_REF is missing.');
+  }
+
+  const generatedCodeBranch = `generated/${process.env.HEAD_REF || headRef}`;
+
+  if (!(await branchExists(generatedCodeBranch))) {
+    console.log(`No branch named '${generatedCodeBranch}' was found.`);
+
+    return;
+  }
+
+  // Delete previous generations to avoid conflicts and out of date code
+  if (await branchExists(generatedCodeBranch)) {
+    console.log(`Deleting generated branch: '${generatedCodeBranch}'`);
+
+    await run(`git fetch origin ${generatedCodeBranch}`);
+    await run(`git push -d origin ${generatedCodeBranch}`);
+  }
+}
+
+if (process.env.HEAD_REF) {
+  cleanGeneratedBranch();
+}
