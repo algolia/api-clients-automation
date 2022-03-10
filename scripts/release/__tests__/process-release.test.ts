ffa8a447457bf307ebbe67016fe83fd826f41cae
Eunjae Lee <karis612@gmail.com>
2022-03-10 11:41:34 +0100

    chore: update test case

--- scripts/release/__tests__/process-release.test.ts
+++ scripts/release/__tests__/process-release.test.ts
@@ -2,18 +2,15 @@ import { getVersionsToRelease, getLangsToUpdateRepo } from '../process-release';
 
 describe('process release', () => {
   it('gets versions to release', () => {
-    expect(
-      getVersionsToRelease(`
-## Version Changes
+    const versions = getVersionsToRelease(`
+    ## Version Changes
+    
+    - [x] javascript: v1.0.0 -> v1.1.0
+    `);
 
-- [x] javascript: v1.0.0 -> v1.1.0
-`)
-    ).toEqual({
-      javascript: {
-        current: '1.0.0',
-        next: '1.1.0',
-      },
-    });
+    expect(Object.keys(versions)).toEqual(['javascript']);
+    expect(versions.javascript.current).toEqual('1.0.0');
+    expect(versions.javascript.next).toEqual('1.1.0');
   });
 
   it('gets langs to update', () => {
