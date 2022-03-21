be0aef90067006a061221fba17be1abf3c939dfc
Clément Vannicatte <20689156+shortcuts@users.noreply.github.com>
2022-03-21 11:57:10 +0100

    feat(javascript): add cache layer

--- clients/algoliasearch-client-javascript/packages/client-common/src/createMemoryCache.ts
+++ clients/algoliasearch-client-javascript/packages/client-common/src/createMemoryCache.ts
@@ -1,27 +1,42 @@
-import type { Cache } from './types';
+import type { Cache, CacheEvents, MemoryCacheOptions } from './types';
 
-export function createMemoryCache(): Cache {
+export function createMemoryCache(
+  options: MemoryCacheOptions = { serializable: true }
+): Cache {
   let cache: Record<string, any> = {};
 
   return {
-    async get<TValue>(
+    get<TValue>(
       key: Record<string, any> | string,
-      defaultValue: () => Promise<TValue>
+      defaultValue: () => Promise<TValue>,
+      events: CacheEvents<TValue> = {
+        miss: (): Promise<void> => Promise.resolve(),
+      }
     ): Promise<TValue> {
       const keyAsString = JSON.stringify(key);
 
       if (keyAsString in cache) {
-        return Promise.resolve(cache[keyAsString]);
+        return Promise.resolve(
+          options.serializable
+            ? JSON.parse(cache[keyAsString])
+            : cache[keyAsString]
+        );
       }
 
-      return await defaultValue();
+      const promise = defaultValue();
+
+      return promise
+        .then((value: TValue) => events.miss(value))
+        .then(() => promise);
     },
 
     set<TValue>(
       key: Record<string, any> | string,
       value: TValue
     ): Promise<TValue> {
-      cache[JSON.stringify(key)] = value;
+      cache[JSON.stringify(key)] = options.serializable
+        ? JSON.stringify(value)
+        : value;
 
       return Promise.resolve(value);
     },
