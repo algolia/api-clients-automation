81748696faace5af1d13da9ae5ba6428c343dc16
Pierre Millot <pierre.millot@algolia.com>
2022-03-31 11:17:12 +0200

    feat(javascript): add custom generator to fix customRequest

--- generators/src/main/java/com/algolia/codegen/Utils.java
+++ generators/src/main/java/com/algolia/codegen/Utils.java
@@ -1,7 +1,22 @@
 package com.algolia.codegen;
 
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+import org.openapitools.codegen.CodegenOperation;
+
 public class Utils {
+  public static final Set<String> CUSTOM_METHOD = Sets.newHashSet("del", "get", "post", "put");
+
   public static String capitalize(String str) {
     return str.substring(0, 1).toUpperCase() + str.substring(1);
+  }
+
+  public static CodegenOperation specifyCustomRequest(CodegenOperation ope) {
+    if (CUSTOM_METHOD.contains(ope.nickname)) {
+      ope.vendorExtensions.put("x-is-custom-request", true);
+    }
+    return ope;
   }
 }
