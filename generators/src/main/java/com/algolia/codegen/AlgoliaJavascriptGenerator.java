e56272760a8c105d0182a920fe6674d76dff6d3b
Pierre Millot <pierre.millot@algolia.com>
2022-04-05 11:30:16 +0200

    handle special case for js

--- generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.openapitools.codegen.CodegenOperation;
 import org.openapitools.codegen.languages.TypeScriptNodeClientCodegen;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.ArraySchema;
+import org.openapitools.codegen.utils.ModelUtils;
 
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.servers.Server;
@@ -17,5 +20,13 @@ public class AlgoliaJavascriptGenerator extends TypeScriptNodeClientCodegen {
   @Override
   public CodegenOperation fromOperation(String path, String httpMethod, Operation operation, List<Server> servers) {
     return Utils.specifyCustomRequest(super.fromOperation(path, httpMethod, operation, servers));
+  }
+
+  @Override
+  public String getTypeDeclaration(Schema p) {
+    String type = super.getTypeDeclaration(p);
+    // openapi generator is wrong, 'object' is not a fit all object, we need 'any'
+    // we use replace because there might be more to this type, like '| undefined'
+    return type.replace("{ [key: string]: object; }", "Record<String, any>");
   }
 }
