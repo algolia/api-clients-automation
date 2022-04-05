0455755be2c70c2e69828c539b1a26a1d3ab5e06
Pierre Millot <pierre.millot@algolia.com>
2022-04-05 11:36:59 +0200

    real fix

--- generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
@@ -5,9 +5,7 @@ import java.util.List;
 import org.openapitools.codegen.CodegenOperation;
 import org.openapitools.codegen.languages.TypeScriptNodeClientCodegen;
 import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.media.ArraySchema;
-import org.openapitools.codegen.utils.ModelUtils;
-
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.servers.Server;
 
@@ -23,10 +21,10 @@ public class AlgoliaJavascriptGenerator extends TypeScriptNodeClientCodegen {
   }
 
   @Override
-  public String getTypeDeclaration(Schema p) {
-    String type = super.getTypeDeclaration(p);
+  protected String getParameterDataType(Parameter parameter, Schema p) {
+    String type = super.getParameterDataType(parameter, p);
     // openapi generator is wrong, 'object' is not a fit all object, we need 'any'
     // we use replace because there might be more to this type, like '| undefined'
-    return type.replace("{ [key: string]: object; }", "Record<String, any>");
+    return type.replace("{ [key: string]: object; }", "Record<string, any>");
   }
 }
