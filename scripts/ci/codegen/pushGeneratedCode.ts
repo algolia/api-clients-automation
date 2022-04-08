b73db924957df150b3488708fc793a15cc18434c
Clément Vannicatte <vannicattec@gmail.com>
2022-04-08 14:12:56 +0200

    update gen commit

--- scripts/ci/codegen/pushGeneratedCode.ts
+++ scripts/ci/codegen/pushGeneratedCode.ts
@@ -59,7 +59,7 @@ export async function pushGeneratedCode(): Promise<void> {
   }
 
   const commitMessage =
-    await run(`git show -s ${baseBranch} --format="Generated code for commit %H.
+    await run(`git show -s ${baseBranch} --format="chore: generated code for commit %H.
 
 Co-authored-by: %an <%ae>"`);
 
