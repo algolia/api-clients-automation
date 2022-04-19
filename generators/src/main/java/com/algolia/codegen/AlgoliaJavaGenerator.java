37babc808651b511c7cd8f38b9a65522f9c00f01
Pierre Millot <pierre.millot@algolia.com>
2022-04-19 14:33:14 +0200

    fix(java): move code to src folder

--- generators/src/main/java/com/algolia/codegen/AlgoliaJavaGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaJavaGenerator.java
@@ -155,15 +155,15 @@ public class AlgoliaJavaGenerator extends JavaClientCodegen {
         List<HashMap<String, String>> listOneOf = new ArrayList();
 
         for (String iterateModel : model.oneOf) {
-          HashMap<String, String> hashMapOneOf = new HashMap();
+          HashMap<String, String> oneOfModel = new HashMap();
 
-          hashMapOneOf.put("type", iterateModel);
-          hashMapOneOf.put(
+          oneOfModel.put("type", iterateModel);
+          oneOfModel.put(
             "name",
             iterateModel.replace("<", "").replace(">", "")
           );
 
-          listOneOf.add(hashMapOneOf);
+          listOneOf.add(oneOfModel);
         }
 
         model.vendorExtensions.put("x-is-one-of-interface", true);
@@ -189,7 +189,7 @@ public class AlgoliaJavaGenerator extends JavaClientCodegen {
       supportingFiles.add(
         new SupportingFile(
           "EchoResponse.mustache",
-          "algoliasearch-core/com/algolia/utils/echo",
+          sourceFolder + "/com/algolia/utils/echo",
           "EchoResponse" + clientName + ".java"
         )
       );
