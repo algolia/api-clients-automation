02cc8895345557c539e3f275e9b519f7c6374f74
Clément Vannicatte <vannicattec@gmail.com>
2022-05-20 15:45:34 +0200

    remove unused import

--- playground/javascript/node/search.ts
+++ playground/javascript/node/search.ts
@@ -1,7 +1,6 @@
 import { searchClient } from '@experimental-api-clients-automation/client-search';
 import { ApiError } from '@experimental-api-clients-automation/client-common';
 import dotenv from 'dotenv';
-import { echoRequester } from '@experimental-api-clients-automation/requester-node-http';
 
 dotenv.config({ path: '../../.env' });
 
