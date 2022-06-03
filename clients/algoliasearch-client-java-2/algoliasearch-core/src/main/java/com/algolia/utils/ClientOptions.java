a72f61ae11e432b1ac3a55da55ecbe13fa1d4cda
Pierre Millot <pierre.millot@algolia.com>
2022-06-03 15:04:34 +0200

    feat(java): use ClientOptions in client constructor

---
+++ clients/algoliasearch-client-java-2/algoliasearch-core/src/main/java/com/algolia/utils/ClientOptions.java
@@ -0,0 +1,42 @@
+package com.algolia.utils;
+
+import com.algolia.utils.retry.StatefulHost;
+import java.util.List;
+
+public class ClientOptions {
+
+  private Requester requester;
+  private AlgoliaAgent.Segment[] algoliaAgentSegments;
+  private List<StatefulHost> hosts;
+
+  public ClientOptions() {
+    this.requester = requester;
+  }
+
+  public Requester getRequester() {
+    return this.requester;
+  }
+
+  public ClientOptions setRequester(Requester requester) {
+    this.requester = requester;
+    return this;
+  }
+
+  public AlgoliaAgent.Segment[] getAlgoliaAgentSegments() {
+    return this.algoliaAgentSegments;
+  }
+
+  public ClientOptions setAlgoliaAgentSegments(AlgoliaAgent.Segment[] algoliaAgentSegments) {
+    this.algoliaAgentSegments = algoliaAgentSegments;
+    return this;
+  }
+
+  public List<StatefulHost> getHosts() {
+    return this.hosts;
+  }
+
+  public ClientOptions setHosts(List<StatefulHost> hosts) {
+    this.hosts = hosts;
+    return this;
+  }
+}
