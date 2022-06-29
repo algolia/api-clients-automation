d09b3e7004885c9fb53427415ff454d32f8f3c43
Clément Vannicatte <vannicattec@gmail.com>
2022-06-29 10:05:24 +0200

    fix(javascript): add class-proposal plugin for `client-common`

--- clients/algoliasearch-client-javascript/packages/client-common/src/transporter/errors.ts
+++ clients/algoliasearch-client-javascript/packages/client-common/src/transporter/errors.ts
@@ -1,12 +1,14 @@
 import type { Response, StackFrame } from '../types';
 
-class ErrorWithStackTrace extends Error {
+export class ErrorWithStackTrace extends Error {
   stackTrace: StackFrame[];
+  name: string;
 
-  constructor(message: string, stackTrace: StackFrame[]) {
+  constructor(message: string, stackTrace: StackFrame[], name: string) {
     super(message);
     // the array and object should be frozen to reflect the stackTrace at the time of the error
     this.stackTrace = stackTrace;
+    this.name = name ?? 'ErrorWithStackTrace';
   }
 }
 
@@ -14,7 +16,8 @@ export class RetryError extends ErrorWithStackTrace {
   constructor(stackTrace: StackFrame[]) {
     super(
       'Unreachable hosts - your application id may be incorrect. If the error persists, contact support@algolia.com.',
-      stackTrace
+      stackTrace,
+      'RetryError'
     );
   }
 }
@@ -23,16 +26,18 @@ export class ApiError extends ErrorWithStackTrace {
   status: number;
 
   constructor(message: string, status: number, stackTrace: StackFrame[]) {
-    super(message, stackTrace);
+    super(message, stackTrace, 'ApiError');
     this.status = status;
   }
 }
 
 export class DeserializationError extends Error {
   response: Response;
+  name: string;
 
   constructor(message: string, response: Response) {
     super(message);
     this.response = response;
+    this.name = 'DeserializationError';
   }
 }
