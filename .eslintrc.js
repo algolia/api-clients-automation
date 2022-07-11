1112e5001e0081e7833ae025663719a997b642a1
Clément Vannicatte <vannicattec@gmail.com>
2022-07-11 10:58:50 +0200

    eslint at monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -127,6 +127,27 @@ module.exports = {
         ],
       },
     },
+    // JS client rules
+    {
+      files: ['clients/algoliasearch-client-javascript/packages/**/*.ts'],
+      rules: {
+        // For a wider browser support (IE>=11), we forbid those two
+        'no-restricted-syntax': [
+          'error',
+          {
+            selector: "LogicalExpression[operator='??']",
+            message:
+              'For wider browser support, nullish coalescing operator is not allowed.',
+          },
+          {
+            selector: 'ChainExpression',
+            message:
+              'For wider browser support, optional chaining is not allowed.',
+          },
+        ],
+        '@typescript-eslint/prefer-optional-chain': 0,
+      },
+    },
     {
       files: ['*.json'],
 
