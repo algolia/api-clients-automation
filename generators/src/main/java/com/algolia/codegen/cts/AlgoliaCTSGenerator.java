49c4df11cbec2904f630ed57e3d1fc743b4a9e0b
Pierre Millot <pierre.millot@algolia.com>
2022-10-24 08:52:39 +0200

    skip CTS

--- generators/src/main/java/com/algolia/codegen/cts/AlgoliaCTSGenerator.java
+++ generators/src/main/java/com/algolia/codegen/cts/AlgoliaCTSGenerator.java
@@ -44,6 +44,11 @@ public class AlgoliaCTSGenerator extends DefaultCodegen {
     client = (String) additionalProperties.get("client");
     ctsManager = CTSManagerFactory.getManager(language, client);
 
+    if (ctsManager == null) {
+      // skip the generation
+      System.exit(0);
+    }
+
     String outputFolder = Utils.getClientConfigField(language, "tests", "outputFolder");
     String extension = Utils.getClientConfigField(language, "tests", "extension");
 
