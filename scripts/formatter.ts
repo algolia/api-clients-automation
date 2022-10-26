7bed7d9a69baa45a067f569f4a81d5c7cbcff4e3
Pierre Millot <pierre.millot@algolia.com>
2022-10-26 14:17:46 +0200

    add formatter

--- scripts/formatter.ts
+++ scripts/formatter.ts
@@ -10,30 +10,40 @@ export async function formatter(
     { text: `formatting '${language}'`, indent: 4 },
     verbose
   ).start();
-  let cmd = '';
   switch (language) {
     case 'javascript':
-      cmd = `yarn eslint --ext=ts,json ${folder} --fix --no-error-on-unmatched-pattern`;
+      await run(
+        `yarn eslint --ext=ts,json ${folder} --fix --no-error-on-unmatched-pattern`,
+        { verbose }
+      );
       break;
     case 'java':
-      cmd = `find ${folder} -type f -name "*.java" | xargs java --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
+      await run(
+        `find ${folder} -type f -name "*.java" | xargs java --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
         --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
         --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
         --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
         --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
         -jar /tmp/java-formatter.jar -r \
-        && yarn prettier --write ${folder}`;
+        && yarn prettier --write ${folder}`,
+        { verbose }
+      );
       break;
     case 'php':
       await runComposerUpdate(verbose);
-      cmd = `yarn run prettier ${folder} --write \
+      await run(
+        `yarn run prettier ${folder} --write \
             && PHP_CS_FIXER_IGNORE_ENV=1 ${
               CI ? 'php' : 'php8'
-            } clients/algoliasearch-client-php/vendor/bin/php-cs-fixer fix ${folder} --using-cache=no --allow-risky=yes`;
+            } clients/algoliasearch-client-php/vendor/bin/php-cs-fixer fix ${folder} --using-cache=no --allow-risky=yes`,
+        { verbose }
+      );
+      break;
+    case 'go':
+      await run(`go fmt ./...`, { verbose, cwd: folder });
       break;
     default:
       return;
   }
-  await run(cmd, { verbose });
   spinner.succeed();
 }
