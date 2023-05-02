d43683058bd4da1ec344c4721fbd0e8db091ba26
Mehmet Ali Gok <mehmetaligok95@gmail.com>
2023-05-02 13:04:08 +0200

    fix(docker): remove debug port

--- scripts/docker/mount.sh
+++ scripts/docker/mount.sh
@@ -2,4 +2,4 @@
 
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)/../.."
 
-docker run -d -p 1043:1043 -it --name api-clients-automation --mount type=bind,source=$ROOT/,target=/app api-clients-automation
+docker run -d -it --name api-clients-automation --mount type=bind,source=$ROOT/,target=/app api-clients-automation
