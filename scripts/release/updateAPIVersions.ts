16b38c38125fa7808e90cf4180a09ff236c5e797
Mouaad Aallam <mouaad.aallam@algolia.com>
2023-06-20 12:42:30 +0200

    chore: fix post scripts

--- scripts/release/updateAPIVersions.ts
+++ scripts/release/updateAPIVersions.ts
@@ -219,15 +219,18 @@ export async function updateAPIVersions(
 export async function updateDartPackages(): Promise<void> {
   const cwd = getLanguageFolder('dart');
 
-  // Generate packages versions and changelogs.
+  console.log("Generate dart packages versions and changelogs")
   await run(
     `(cd ${cwd} && melos version --no-git-tag-version --published --yes)`
   );
 
+  console.log("Update packages configs based on generated versions")
   for (const gen of Object.values(GENERATORS)) {
     if (gen.language === 'dart') {
       const { additionalProperties } = gen;
-      const newVersion = await getPubspecVersion(`${gen.output}/pubspec.yaml`);
+      const newVersion = await getPubspecVersion(
+        `../${gen.output}/pubspec.yaml`
+      );
       if (!newVersion) {
         throw new Error(`Failed to bump '${gen.packageName}'.`);
       }
