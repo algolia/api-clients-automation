404afcf6262b518495b5295948a0c88f2e91aee5
Mouaad Aallam <mouaad.aallam@algolia.com>
2023-06-20 15:53:14 +0200

    use vars for args

--- clients/algoliasearch-client-dart/sync.sh
+++ clients/algoliasearch-client-dart/sync.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+if [[ "$(uname)" == "Darwin" ]]; then
+  sed_arg=('-i' '' '-e')
+else
+  sed_arg=('-i' '-e')
+fi
+
 for dir in packages/*/ ; do
   dir=${dir%/}
-  version=$(grep "^version:" "${dir}/pubspec.yaml" | sed 's/version: //') &&
-  if [[ "$(uname)" == "Darwin" ]]; then \
-    sed -i '' -e "s/^const packageVersion = .*;$/const packageVersion = '$version';/" "${dir}/lib/src/version.dart";
-  else
-    sed -i -e "s/^const packageVersion = .*;$/const packageVersion = '$version';/" "${dir}/lib/src/version.dart";
-  fi;
+  version=$(grep "^version:" "${dir}/pubspec.yaml" | sed 's/version: //')
+  sed "${sed_arg[@]}" "s/^const packageVersion = .*;$/const packageVersion = '$version';/" "${dir}/lib/src/version.dart";
 done
