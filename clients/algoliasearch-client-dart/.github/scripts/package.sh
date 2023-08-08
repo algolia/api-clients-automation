0ecf5a138d7032ede819b53c767af6c4184aba66
Mouaad Aallam <mouaad.aallam@algolia.com>
2023-08-08 11:03:49 +0200

    publish all packages on tag

--- clients/algoliasearch-client-dart/.github/scripts/package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TAG_NAME=${GITHUB_REF#refs/tags/}
-PACKAGE_NAME=${TAG_NAME%-*}
-if [ "$PACKAGE_NAME" = "algoliasearch" ]; then
-  echo "package-dir=packages/algoliasearch" >> "$GITHUB_OUTPUT"
-else
-  echo "package-dir=packages/client_${PACKAGE_NAME}" >> "$GITHUB_OUTPUT"
-fi
