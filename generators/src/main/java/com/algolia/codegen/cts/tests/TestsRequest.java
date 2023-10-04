a1c15faa8ca4d9962dd82cd2ecfe3e8b179f36c0
Pierre Millot <pierre.millot@algolia.com>
2023-10-04 15:38:05 +0200

    fix for dart

--- generators/src/main/java/com/algolia/codegen/cts/tests/TestsRequest.java
+++ generators/src/main/java/com/algolia/codegen/cts/tests/TestsRequest.java
@@ -93,6 +93,11 @@ public class TestsRequest extends TestsGenerator {
             req.request.body = req.request.body.replace("%", "%%");
           }
 
+          // For dart, same thing but for $
+          if (language.equals("dart") && req.request.body != null) {
+            req.request.body = req.request.body.replace("$", "\\$");
+          }
+
           // In a case of a `GET` or `DELETE` request, we want to assert if the body
           // is correctly parsed (absent from the payload)
           if (req.request.method.equals("GET") || req.request.method.equals("DELETE")) {
