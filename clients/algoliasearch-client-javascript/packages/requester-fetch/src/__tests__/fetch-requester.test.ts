a8b0e128f65a9b8e67bb4b7f44109f3a8a8e06e9
Pierre Millot <pierre.millot@algolia.com>
2023-10-27 15:06:44 +0200

    try with custom server

--- clients/algoliasearch-client-javascript/packages/requester-fetch/src/__tests__/fetch-requester.test.ts
+++ clients/algoliasearch-client-javascript/packages/requester-fetch/src/__tests__/fetch-requester.test.ts
@@ -130,7 +130,7 @@ describe('timeout handling', () => {
     const response = await requester.send({
       ...timeoutRequest,
       connectTimeout: 1000,
-      url: 'http://www.google.com:81',
+      url: 'http://localhost:1111/connection_timeout',
     });
 
     const now = Date.now();
@@ -145,7 +145,7 @@ describe('timeout handling', () => {
     const response = await requester.send({
       ...timeoutRequest,
       connectTimeout: 2000,
-      url: 'http://www.google.com:81',
+      url: 'http://localhost:1111/connection_timeout',
     });
 
     const now = Date.now();
