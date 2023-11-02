78f4dda0e8ed08bdb75b3dbcde723527f4418405
Mouaad Aallam <mouaad.aallam@algolia.com>
2023-11-02 17:19:36 +0100

    wip

--- generators/src/main/java/com/algolia/codegen/AlgoliaPythonGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaPythonGenerator.java
@@ -1,5 +1,6 @@
 package com.algolia.codegen;
 
+import java.io.File;
 import org.openapitools.codegen.SupportingFile;
 import org.openapitools.codegen.languages.PythonClientCodegen;
 
@@ -17,7 +18,7 @@ public class AlgoliaPythonGenerator extends PythonClientCodegen {
     setPackageName("algoliasearch");
     setApiNameSuffix(Utils.API_SUFFIX);
     setModelPackage(client);
-    setApiPackage(""); // root
+    setApiPackage(client);
     super.processOpts();
 
     // Generation notice, added on every generated files
@@ -38,5 +39,11 @@ public class AlgoliaPythonGenerator extends PythonClientCodegen {
 
     supportingFiles.add(new SupportingFile("pyproject.mustache", "", "pyproject.toml"));
     supportingFiles.add(new SupportingFile("version.mustache", packagePath(), "version.py"));
+    String packagePath = packagePath() + File.separatorChar + modelPackage;
+    supportingFiles.add(new SupportingFile("__init__.mustache", packagePath, "__init__.py"));
+    // supportingFiles.add(new SupportingFile("__init__model.mustache", modelPath, "__init__.py"));
+    // String apiPath = packagePath() + File.separatorChar + apiPackage.replace('.',
+    // File.separatorChar);
+    // supportingFiles.add(new SupportingFile("__init__api.mustache", apiPath, "__init__.py"));
   }
 }
