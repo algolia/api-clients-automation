5c9868c9d5ae1f173ef3ca2a9d0f8c074339b6f3
Pierre Millot <pierre.millot@algolia.com>
2023-11-21 11:33:10 +0100

    generate the version file

--- scripts/website/build.sh
+++ scripts/website/build.sh
@@ -3,7 +3,10 @@
 # treat website as independant yarn project
 touch website/yarn.lock
 
-DOCKER=true yarn cli build specs all -s 
+DOCKER=true yarn cli build specs all
+
+# Generate the file with constants
+cat config/clients.config.json| jq -r '"export const versions = \(map_values(.packageVersion))"' > website/src/generated/variables.js
 
 # install website deps and build
 cd website && yarn install && yarn build
--- website/src/variables.js
+++
@@ -1,5 +0,0 @@
-export const versions = {
-  "javascript": "0.0.1",
-  "java": "55555555"
-};
-
