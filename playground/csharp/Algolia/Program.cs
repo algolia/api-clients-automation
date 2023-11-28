748fab65175a4cb4a2de557a3a842cb013a0e2c3
Morgan Leroi <morgan.leroi@gmail.com>
2023-11-28 16:43:38 +0100

    add playground

---
+++ playground/csharp/Algolia/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Algolia.Search.Search.Api;
+using Algolia.Search.Search.Client;
+
+Console.WriteLine("Algolia Search Client Playground for C#");
+
+
+
