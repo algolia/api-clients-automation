56b6583cdfee053bcb6b32ad95c67382031f9b6e
shortcuts <vannicattec@gmail.com>
2023-11-28 20:51:27 +0100

    feat: allow more request options

--- clients/algoliasearch-client-python/algoliasearch/http/request_options.py
+++ clients/algoliasearch-client-python/algoliasearch/http/request_options.py
@@ -22,18 +22,6 @@ class RequestOptions(object):
         self.timeouts = timeouts
         self.data = data
 
-    def __setitem__(self, option: str, value: Any) -> None:
-        if option.upper() in [
-            header.upper() for header in AllowedRequestsOptionsParams.HEADERS
-        ]:
-            self.headers[option] = str(value)
-        elif option in AllowedRequestsOptionsParams.QUERY_PARAMETERS:
-            self.query_parameters[option] = value
-        elif option in AllowedRequestsOptionsParams.TIMEOUTS:
-            self.timeouts[option] = value
-        else:
-            self.data[option] = value
-
     def create(
         config: Config, options: Optional[Union[Self, Dict[str, Any]]] = None
     ) -> Self:
@@ -55,27 +43,3 @@ class RequestOptions(object):
                 request_options[option] = value
 
         return request_options
-
-
-class AllowedRequestsOptionsParams(object):
-    HEADERS = (
-        "Content-type",
-        "User-Agent",
-        "X-Algolia-User-ID",
-        "X-Algolia-UserToken",
-        "X-Forwarded-For",
-    )
-
-    QUERY_PARAMETERS = (
-        "createIfNotExists",
-        "forwardToReplicas",
-        "replaceExistingSynonyms",
-        "clearExistingRules",
-        "getVersion",
-    )
-
-    TIMEOUTS = (
-        "readTimeout",
-        "writeTimeout",
-        "connectTimeout",
-    )
