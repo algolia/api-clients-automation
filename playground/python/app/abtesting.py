f990078983cf63f136f358604aa198de2446638e
Clément Vannicatte <vannicattec@gmail.com>
2023-11-30 10:07:27 +0100

    feat(python): add Algolia transporter, response deserializer and config setup (#2306)

--- playground/python/app/abtesting.py
+++ playground/python/app/abtesting.py
@@ -1,10 +1,23 @@
+from asyncio import run
+
 from algoliasearch.abtesting import __version__
 from algoliasearch.abtesting.client import AbtestingClient
 
 
-def main():
+async def main():
     print("AbtestingClient version", __version__)
 
     client = AbtestingClient()
 
     print("client initialized", client)
+
+    try:
+        response = await client.stop_ab_test(id=10)
+
+        print(response)
+    finally:
+        await client.close()
+
+        print("client closed", client)
+
+run(main())
--- playground/python/app/personalization.py
+++ playground/python/app/personalization.py
@@ -1,10 +1,23 @@
+from asyncio import run
+
 from algoliasearch.personalization import __version__
 from algoliasearch.personalization.client import PersonalizationClient
 
 
-def main():
+async def main():
     print("PersonalizationClient version", __version__)
 
-    client = PersonalizationClient()
+    client = PersonalizationClient.create("FOO", "BAR")
 
     print("client initialized", client)
+
+    try:
+        response = await client.delete_user_profile(user_token="foo")
+
+        print(response)
+    finally:
+        await client.close()
+
+        print("client closed", client)
+
+run(main())
