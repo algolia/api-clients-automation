fba7e5a9abec089ce442d9130f170f95d92de7e8
Pierre Millot <pierre.millot@algolia.com>
2023-12-20 14:34:44 +0100

    chore(cli): add apic alias to bypass yarn scripts

---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This scripts installs the apic cli shortcut, instead of doing `docker docker ...` which takes a long time to start
+# You can install this by running `source scripts/install.sh` or by adding this to your .bashrc/.zshrc:
+# [[ -s "$HOME/<path to api-clients-automation>/scripts/install.sh" ]] && source "$HOME/<path to api-clients-automation>/scripts/install.sh"
+
+export apic() {
+  docker exec -it api-clients-automation bash -lc "cd scripts && NODE_NO_WARNINGS=1 node dist/scripts/cli/index.js $*";
+}
+
