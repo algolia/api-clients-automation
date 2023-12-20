7efbacca536fe27d06d3a190c9febd97b05964fe
Pierre Millot <pierre.millot@algolia.com>
2023-12-20 15:30:14 +0100

    revert playground

--- playground/ruby/search.rb
+++ playground/ruby/search.rb
@@ -6,6 +6,3 @@ Dotenv.load('../.env')
 client = Algolia::SearchClient.create(ENV['ALGOLIA_APPLICATION_ID'], ENV['ALGOLIA_ADMIN_KEY'])
 res = client.search_single_index('actors', Algolia::Search::SearchParamsObject.new(query: 'john'))
 puts res
-
-res = client.get_sources
-puts res
