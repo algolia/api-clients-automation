b5e729f087c19aa29971cc24fd27c08a705d303c
Pierre Millot <pierre.millot@algolia.com>
2023-12-20 14:38:57 +0100

    typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This scripts installs the apic cli shortcut, instead of doing `docker docker ...` which takes a long time to start
+# This scripts installs the apic cli shortcut, instead of doing `yarn docker ...` which takes a long time to start
 # You can install this by running `source scripts/install.sh` or by adding this to your .bashrc/.zshrc:
 # [[ -s "$HOME/<path to api-clients-automation>/scripts/install.sh" ]] && source "$HOME/<path to api-clients-automation>/scripts/install.sh"
 
