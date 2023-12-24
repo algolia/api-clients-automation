aeeb77d948fb4e12551b838cdf73391ad237d804
Pierre Millot <pierre.millot@algolia.com>
2023-12-24 10:49:16 +0100

    no minify ?

--- scripts/ci/actions/restore-artifacts/src/index.ts
+++ scripts/ci/actions/restore-artifacts/src/index.ts
@@ -1,12 +1,18 @@
+import { DefaultArtifactClient } from '@actions/artifact';
 import * as core from '@actions/core';
 
-function run(): void {
+async function run(): Promise<void> {
   try {
     const actionType = core.getInput('type');
     const languages = core.getInput('languages');
 
     core.info(`actionType: ${actionType}`);
     core.info(`languages: ${languages}`);
+
+    const artifact = new DefaultArtifactClient();
+    const artifacts = await artifact.listArtifacts();
+
+    core.info(`artifacts: ${artifacts}`);
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
