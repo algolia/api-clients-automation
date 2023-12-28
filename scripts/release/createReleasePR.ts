f30af40553a3979ffa3119d1963b39b5cff8f886
Pierre Millot <pierre.millot@algolia.com>
2023-12-28 15:19:22 +0100

    fix com

--- scripts/release/createReleasePR.ts
+++ scripts/release/createReleasePR.ts
@@ -179,6 +179,7 @@ export function getNextVersion(current: string, releaseType: semver.ReleaseType 
   // python alpha releases have a pattern like X.Y.ZaN
   // where a means alpha and N can be any digit representing the alpha version
   // see https://peps.python.org/pep-0440/
+  // It also support ruby alpha like `X.Y.Z.alpha.N`
   if (releaseType !== 'major' && /\d\.\d\.\d\.?a(lpha\.)?\d+$/.test(current)) {
     nextVersion = current.replace(/\d+$/, (match) => `${parseInt(match, 10) + 1}`);
   } else if (current.endsWith('-SNAPSHOT')) {
