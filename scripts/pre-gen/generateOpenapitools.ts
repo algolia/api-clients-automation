0b38b6feb03b59dc822a499667a7648e69b07163
shortcuts <vannicattec@gmail.com>
2024-01-05 16:15:53 +0100

    fix: openapitools.json references

--- scripts/pre-gen/generateOpenapitools.ts
+++ scripts/pre-gen/generateOpenapitools.ts
@@ -9,27 +9,21 @@ const AVAILABLE_CUSTOM_GEN = Object.values(clientsConfig)
   .map((gen) => ('customGenerator' in gen ? gen.customGenerator : null))
   .filter(Boolean);
 
-const baseOpenapitoolsConfig = {
-  'generator-cli': {
-    version: '7.2.0',
-  },
-};
-
 /**
  * Create an on the fly openapitools.json file with default options for all generators.
  *
  * Defaults options are used to
  * - Set config path.
  */
-export async function generateOpenapitools(generators: Generator[]): Promise<void> {
-  const result = {};
-  for (const { key, client, language, additionalProperties, ...rest } of generators) {
+export async function generateOpenapitools(gens: Generator[]): Promise<void> {
+  const generators = {};
+  for (const { key, client, language, additionalProperties, ...rest } of gens) {
     const templateDir =
       language === 'javascript'
         ? `#{cwd}/templates/${language}/clients`
         : `#{cwd}/templates/${language}/`;
 
-    result[key] = {
+    generators[key] = {
       config: '#{cwd}/openapitools.json',
       gitHost: 'github.com',
       gitUserId: 'algolia',
@@ -39,21 +33,20 @@ export async function generateOpenapitools(generators: Generator[]): Promise<voi
       generatorName: AVAILABLE_CUSTOM_GEN.includes(`algolia-${language}`)
         ? `algolia-${language}`
         : rest.generatorName,
-      ...rest,
       output: `#{cwd}/${rest.output}`,
       additionalProperties: {
         client,
-        ...additionalProperties,
       },
     };
   }
+
   await writeFile(
     toAbsolutePath('openapitools.json'),
     JSON.stringify(
       {
         'generator-cli': {
-          version: baseOpenapitoolsConfig['generator-cli'].version,
-          generators: result,
+          version: '7.2.0',
+          generators,
         },
       },
       null,
