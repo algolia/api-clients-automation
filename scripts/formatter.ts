6c4c999758b61981f8e541a0a321362a379fe6b0
Morgan Leroi <morgan.leroi@gmail.com>
2024-01-15 15:43:24 +0100

    fix snippets

--- scripts/formatter.ts
+++ scripts/formatter.ts
@@ -5,7 +5,7 @@ export async function formatter(language: string, cwd: string): Promise<void> {
   const spinner = createSpinner(`running formatter for '${language}' in '${cwd}'`);
   switch (language) {
     case 'csharp':
-      if (cwd.includes('tests')) {
+      if (cwd.includes('tests') || cwd.includes('snippets')) {
         await run('dotnet format && dotnet-csharpier .', { cwd, language });
       } else {
         await run('dotnet format', { cwd, language });
