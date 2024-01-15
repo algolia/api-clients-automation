23775925c944897d1226886817d1cfabc8467888
Morgan Leroi <morgan.leroi@gmail.com>
2024-01-15 11:53:24 +0100

    add csharpier for unit test snippets format

--- scripts/docker/setup.sh
+++ scripts/docker/setup.sh
@@ -2,4 +2,4 @@
 
 export NODE_VERSION=$(cat .nvmrc)
 find config -name '.*-version' | xargs -I{} sh -c 'l=$(echo "{}" | sed -e "s/-/_/;s/config\/\.//" | tr "[a-z]" "[A-Z]");echo "export $l=$(cat {})"'
-docker compose up -d
+docker compose up -d --build
