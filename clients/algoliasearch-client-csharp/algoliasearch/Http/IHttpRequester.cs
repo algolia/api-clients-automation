7929f65f4e68f6989b1b8d2bbaa4b47447ed4553
Morgan Leroi <morgan.leroi@gmail.com>
2024-01-16 22:51:33 +0100

    review

--- clients/algoliasearch-client-csharp/algoliasearch/Http/IHttpRequester.cs
+++ clients/algoliasearch-client-csharp/algoliasearch/Http/IHttpRequester.cs
@@ -17,6 +17,7 @@ namespace Algolia.Search.Http
     /// <param name="requestTimeout">Request timeout</param>
     /// <param name="connectTimeout">Connect timeout</param>
     /// <param name="ct">Optional cancellation token</param>
+    /// <returns></returns>
     Task<AlgoliaHttpResponse> SendRequestAsync(Request request, TimeSpan requestTimeout, TimeSpan connectTimeout, CancellationToken ct = default);
   }
 }
