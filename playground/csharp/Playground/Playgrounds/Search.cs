94750912e35e20df1ee57e1db2a178e1077faf44
Morgan Leroi <morgan.leroi@gmail.com>
2024-01-23 13:25:13 +0100

    add playground

--- playground/csharp/Playground/Playgrounds/Search.cs
+++ playground/csharp/Playground/Playgrounds/Search.cs
@@ -1,6 +1,10 @@
+using System.Diagnostics;
 using Algolia.Search.Clients;
+using Algolia.Search.Exceptions;
 using Algolia.Search.Models;
 using Algolia.Search.Models.Search;
+using Algolia.Search.Utils;
+using Algolia.Utils;
 using Action = Algolia.Search.Models.Search.Action;
 
 public static class SearchPlayground
@@ -38,7 +42,7 @@ public static class SearchPlayground
 
     // Save multiple objects
     Console.WriteLine("--- Save a multiple objects `BatchAsync` ---");
-    var requests = new List<BatchRequest>()
+    var requests = new List<BatchRequest>
     {
       new(Action.AddObject, new { ObjectID = "test3", value = "batch1", otherValue = "otherValue1" }),
       new(Action.AddObject, new { ObjectID = "test4", value = "batch2", otherValue = "otherValue2" }),
@@ -48,9 +52,19 @@ public static class SearchPlayground
     batch.ObjectIDs.ForEach(Console.WriteLine);
 
     // Browse all objects
-    Console.WriteLine("--- Browse all objects `BrowseAsync` ---");
-    var r = await client.BrowseAsync<TestObject>("test-csharp-new-client");
+    Console.WriteLine("--- Browse all objects, one page `BrowseAsync` ---");
+    var r = await client.BrowseAsync<TestObject>("test-csharp-new-client",
+      new BrowseParams(new BrowseParamsObject { HitsPerPage = 100 }));
     r.Hits.ForEach(h => Console.WriteLine(h.ObjectID));
+
+    // Browse Helper, to fetch all pages
+    Console.WriteLine("--- Browse all objects, all pages `BrowseObjectsAsync` ---");
+    var results = await client.BrowseObjectsAsync<TestObject>("test-csharp-new-client", new BrowseParamsObject
+    {
+      HitsPerPage = 1
+    });
+
+    results.ToList().ForEach(h => Console.WriteLine(h.ObjectID));
 
     // Get Objects
     Console.WriteLine("--- Get Objects `GetObjectsAsync` ---");
@@ -105,6 +119,104 @@ public static class SearchPlayground
     foreach (var tMetisAdditionalProperty in tMetis.AdditionalProperties)
     {
       Console.WriteLine(tMetisAdditionalProperty.Key + " : " + tMetisAdditionalProperty.Value);
+    }
+
+    // Add Synomyms
+    Console.WriteLine("--- Add Synonyms `SaveSynonymsAsync` ---");
+    var synonymsResponse = await client.SaveSynonymsAsync("test-csharp-new-client",
+      new List<SynonymHit>
+      {
+        new()
+        {
+          Type = SynonymType.Onewaysynonym, ObjectID = "tshirt",
+          Synonyms = new List<string> { "tshirt", "shirt", "slipover" }, Input = "tshirt"
+        },
+        new()
+        {
+          Type = SynonymType.Onewaysynonym, ObjectID = "trousers",
+          Synonyms = new List<string> { "trousers", "jeans", "pantaloons" }, Input = "trousers"
+        },
+        new()
+        {
+          Type = SynonymType.Onewaysynonym, ObjectID = "shoes",
+          Synonyms = new List<string> { "shoes", "boots", "sandals" }, Input = "shoes"
+        },
+      }).ConfigureAwait(false);
+
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    var waitResult = await client.WaitForTaskAsync("test-csharp-new-client", synonymsResponse.TaskID)
+      .ConfigureAwait(false);
+    stopwatch.Stop();
+    Console.WriteLine($"Task is now {waitResult.Status}");
+    Console.WriteLine($"Task took {stopwatch.ElapsedMilliseconds}ms");
+
+    // // Search Synomyms
+    // Console.WriteLine("--- Search Synonyms `SearchSynonymsAsync` ---");
+    // var searchSynonymsAsync = await client.SearchSynonymsAsync("test-csharp-new-client", SynonymType.Onewaysynonym, 0,
+    //   1, new SearchSynonymsParams() { Query = "" }).ConfigureAwait(false);
+    // Console.WriteLine(searchSynonymsAsync.Hits.Count);
+
+    // MAX TIMEOUT ??
+
+    // Browse Synonyms
+    // var configuredTaskAwaitable = await client
+    //   .BrowseSynonymsAsync("test-csharp-new-client", SynonymType.Onewaysynonym, new SearchSynonymsParams { Query = "" })
+    //   .ConfigureAwait(false);
+    // configuredTaskAwaitable.ToList().ForEach(s => Console.WriteLine("Found :" + string.Join(',', s.Synonyms)));
+
+    // Add Rule
+    Console.WriteLine("--- Create new Rule `SaveRulesAsync` ---");
+    var saveRulesAsync = await client.SaveRulesAsync("test-csharp-new-client",
+      new List<Rule>
+      {
+        new()
+        {
+          ObjectID = "TestRule1", Description = "Test",
+          Consequence =
+            new Consequence { Promote = new List<Promote> { new(new PromoteObjectID("test3", 1)) } },
+          Conditions = new List<Condition>
+            { new() { Anchoring = Anchoring.Contains, Context = "shoes", Pattern = "test" } }
+        },
+        new()
+        {
+          ObjectID = "TestRule2", Description = "Test",
+          Consequence =
+            new Consequence { Promote = new List<Promote> { new(new PromoteObjectID("test4", 1)) } },
+          Conditions = new List<Condition>
+            { new() { Anchoring = Anchoring.Contains, Context = "shoes", Pattern = "test" } }
+        },
+        new()
+        {
+          ObjectID = "TestRule3", Description = "Test",
+          Consequence =
+            new Consequence { Promote = new List<Promote> { new(new PromoteObjectID("test5", 1)) } },
+          Conditions = new List<Condition>
+            { new() { Anchoring = Anchoring.Contains, Context = "shoes", Pattern = "test" } }
+        }
+      }).ConfigureAwait(false);
+
+    await TaskHelper.Start(async () => await client.WaitForTaskAsync("test-csharp-new-client", saveRulesAsync.TaskID));
+
+    Console.WriteLine("--- Error Handling ---");
+    try
+    {
+      await client.SaveRulesAsync("test-csharp-new-client",
+        new List<Rule>
+        {
+          new()
+          {
+            ObjectID = "TestRule1", Description = "Test",
+            Consequence =
+              new Consequence { Promote = new List<Promote> { new(new PromoteObjectID("test3", 1)) } },
+            Conditions = new List<Condition>
+              // Error, no Context set
+              { new() { Anchoring = Anchoring.Contains, Context = "shoes"  } }
+          }
+        }).ConfigureAwait(false);
+    } catch (AlgoliaApiException e)
+    {
+      Console.WriteLine($"Message: {e.Message} - Status {e.HttpErrorCode}");
     }
   }
 }
