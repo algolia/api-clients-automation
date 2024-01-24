c233435f57e9cb0136ff0ccee4904e1a351814e1
Morgan Leroi <morgan.leroi@gmail.com>
2024-01-24 17:07:21 +0100

    try to fix dotnet tool

--- scripts/formatter.ts
+++ scripts/formatter.ts
@@ -6,7 +6,10 @@ export async function formatter(language: string, cwd: string): Promise<void> {
   switch (language) {
     case 'csharp':
       if (cwd.includes('tests') || cwd.includes('snippets')) {
-        await run('dotnet format && dotnet-csharpier .', { cwd, language });
+        await run('dotnet format && dotnet tool restore && dotnet dotnet-csharpier .', {
+          cwd,
+          language,
+        });
       } else {
         await run('dotnet format', { cwd, language });
       }
