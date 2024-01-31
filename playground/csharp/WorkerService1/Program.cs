9086f9e6828ea2462f437465d6696ff26c6e02fe
Morgan Leroi <morgan.leroi@gmail.com>
2024-01-31 14:57:53 +0100

    wip

--- playground/csharp/WorkerService1/Program.cs
+++
@@ -1,8 +0,0 @@
-using WorkerService1;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-builder.Logging.AddFilter("Algolia", LogLevel.Information).AddConsole();
-
-var host = builder.Build();
-host.Run();
