cfb6b7c3ba27e9eb17b24bc745dcc705d82ec040
Morgan Leroi <morgan.leroi@gmail.com>
2024-02-08 15:00:53 +0100

    add discriminator

--- playground/csharp/Playground/Model/TestObject.cs
+++ playground/csharp/Playground/Model/TestObject.cs
@@ -1,7 +1,10 @@
+using System.Runtime.Serialization;
 using Algolia.Search.Models.Search;
 
 public class TestObject : Hit
 {
+  [DataMember(Name = "value")]
   public string? value { get; set; }
+  [DataMember(Name = "otherValue")]
   public string? otherValue { get; set; }
 }
