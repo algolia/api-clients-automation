0ee10516f452f445b815d33aaf2c0f02e577e500
Pierre Millot <pierre.millot@algolia.com>
2024-02-16 01:29:51 +0100

    harmonize the union helper

--- tests/output/ruby/test/helpers.rb
+++ tests/output/ruby/test/helpers.rb
@@ -1,25 +1,20 @@
 def union(expected, received)
-  if !expected.is_a?(Array) and !expected.is_a?(Hash)
-    return expected
-  end
-
-  if received.nil?
-    return nil
-  end
-
-  if expected.is_a?(Array)
+  case expected
+  when Array
     res = []
     expected.each_with_index do |v, i|
       res.push(union(v, received[i]))
     end
 
     return res
-  end
+  when Hash
+    res = {}
+    expected.each do |key, value|
+      res[key] = union(value, received[key])
+    end
 
-  res = {}
-  expected.each do |key, value|
-    res[key] = union(value, received[key])
+    return res
+  else
+    return received
   end
-
-  res
 end
