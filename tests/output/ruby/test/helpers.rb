3dd1ff395489dbfe1ac1651c9fd634065cf6a0c6
Pierre Millot <pierre.millot@algolia.com>
2024-02-16 01:10:30 +0100

    more flexible union for ruby

--- tests/output/ruby/test/helpers.rb
+++ tests/output/ruby/test/helpers.rb
@@ -1,6 +1,4 @@
 def union(expected, received)
-  res = {}
-
   if !expected.is_a?(Array) and !expected.is_a?(Hash)
     return expected
   end
@@ -9,20 +7,18 @@ def union(expected, received)
     return nil
   end
 
-  expected.each do |key, value|
-    if received.key?(key)
-      if value.is_a?(Array)
-        res[key] = [] if res[key].nil?
-
-        value.each_with_index do |v, i|
-          res[key].push(union(v, received[key][i]))
-        end
-      elsif value.is_a?(Hash)
-        res[key] = union(value, received[key])
-      else
-        res[key] = received[key]
-      end
+  if expected.is_a?(Array)
+    res = []
+    expected.each_with_index do |v, i|
+      res.push(union(v, received[i]))
     end
+
+    return res
+  end
+
+  res = {}
+  expected.each do |key, value|
+    res[key] = union(value, received[key])
   end
 
   res
