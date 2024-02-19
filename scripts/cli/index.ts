1081c2839193af98321b7db8eb64072e79fcafff
Thomas Raffray <Fluf22@users.noreply.github.com>
2024-02-19 14:33:10 +0100

    feat(cts): add compression strategy test (#2680)
    Co-authored-by: Pierre Millot <pierre.millot@algolia.com>

--- scripts/cli/index.ts
+++ scripts/cli/index.ts
@@ -5,6 +5,7 @@ import { buildSpecs } from '../buildSpecs.js';
 import { LANGUAGES, setVerbose } from '../common.js';
 import { ctsGenerateMany } from '../cts/generate.js';
 import { runCts } from '../cts/runCts.js';
+import { startTestServer } from '../cts/testServer';
 import { formatter } from '../formatter.js';
 import { generate } from '../generate.js';
 import { playground } from '../playground.js';
@@ -133,6 +134,14 @@ ctsCommand
     setVerbose(Boolean(verbose));
 
     await runCts(language === ALL ? LANGUAGES : [language]);
+  });
+
+ctsCommand
+  .command('server')
+  .description('Start the test servers in standalone mode')
+  .action(async () => {
+    setVerbose(true);
+    await startTestServer();
   });
 
 program
