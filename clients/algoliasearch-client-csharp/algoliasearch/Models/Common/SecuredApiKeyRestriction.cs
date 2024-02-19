b991afd0c4cacb0004451377c8dcf7695cb16362
Morgan Leroi <morgan.leroi@gmail.com>
2024-02-19 17:05:35 +0100

    review

--- clients/algoliasearch-client-csharp/algoliasearch/Models/Common/SecuredApiKeyRestriction.cs
+++ clients/algoliasearch-client-csharp/algoliasearch/Models/Common/SecuredApiKeyRestriction.cs
@@ -1,17 +1,10 @@
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Text.Json.Serialization;
-using Algolia.Search.Http;
 using Algolia.Search.Models.Search;
-using Algolia.Search.Utils;
 
 namespace Algolia.Search.Models.Common;
 
-/// <summary>
-/// Secured Api Key restrictions
-/// </summary>
-public class SecuredApiKeyRestriction
+public partial class SecuredApiKeyRestriction
 {
   /// <summary>
   /// Search query parameters
@@ -41,45 +34,5 @@ public class SecuredApiKeyRestriction
   /// </summary>
   [JsonPropertyName("userToken")]
   public string UserToken { get; set; }
-
-  /// <summary>
-  /// Transforms the restriction into a query string
-  /// </summary>
-  /// <returns></returns>
-  public string ToQueryString()
-  {
-    string restrictionQuery = null;
-    if (Query != null)
-    {
-      restrictionQuery = ToQueryString(Query);
-    }
-
-    var restrictions = ToQueryString(this, nameof(Query));
-    var array = new[] { restrictionQuery, restrictions };
-
-    return string.Join("&", array.Where(s => !string.IsNullOrEmpty(s)));
-  }
-
-  /// <summary>
-  /// Transform a poco (only class of primitive objects) to a query string
-  /// </summary>
-  /// <param name="value"></param>
-  /// <param name="ignoreList"></param>
-  /// <typeparam name="T"></typeparam>
-  /// <returns></returns>
-  private static string ToQueryString<T>(T value, params string[] ignoreList)
-  {
-    var properties = typeof(T).GetTypeInfo()
-      .DeclaredProperties.Where(p =>
-        p.GetValue(value, null) != null && !ignoreList.Contains(p.Name) &&
-        p.GetCustomAttribute<JsonPropertyNameAttribute>() != null)
-      .Select(p => new
-      {
-        propsName = p.GetCustomAttribute<JsonPropertyNameAttribute>().Name,
-        value = ClientUtils.ParameterToString(p.GetValue(value, null))
-      }).ToDictionary(p => p.propsName, p => p.value);
-
-    return properties.ToQueryString();
-  }
 
 }
