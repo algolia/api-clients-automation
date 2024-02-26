9e49dcd22af535b97b36ccf96b4798bdfbc3d47c
Pierre Millot <pierre.millot@algolia.com>
2024-02-26 13:57:53 +0100

    dont generate helpers

--- generators/src/main/java/com/algolia/codegen/AlgoliaJavaGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaJavaGenerator.java
@@ -90,6 +90,7 @@ public class AlgoliaJavaGenerator extends JavaClientCodegen {
   @Override
   public OperationsMap postProcessOperationsWithModels(OperationsMap objs, List<ModelMap> models) {
     OperationsMap operations = super.postProcessOperationsWithModels(objs, models);
+    Helpers.removeHelpers(operations);
     GenericPropagator.propagateGenericsToOperations(operations, models);
     return operations;
   }
