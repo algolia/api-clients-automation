5dd4bfcd50c30a43d741928069bea10687857d2e
shortcuts <vannicattec@gmail.com>
2024-02-28 14:41:04 +0100

    chore: try custom action

--- scripts/website/build.sh
+++ scripts/website/build.sh
@@ -3,8 +3,6 @@
 # treat website as independant yarn project
 touch website/yarn.lock
 
-yarn cli build specs all
-
 # Generate the file with constants
 mkdir -p website/src/generated
 cat config/clients.config.json | jq -r '"export const versions = \(map_values(.packageVersion))"' > website/src/generated/variables.js
