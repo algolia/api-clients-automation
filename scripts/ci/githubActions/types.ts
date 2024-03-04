691482e11e2c2c61c1a563f543e0be91d0b08e86
Clément Vannicatte <vannicattec@gmail.com>
2024-03-04 17:38:55 +0100

    chore: remove custom caching logic (#2826)

--- scripts/ci/githubActions/types.ts
+++ scripts/ci/githubActions/types.ts
@@ -9,61 +9,44 @@ export type CreateMatrix = {
   forClients?: boolean;
 };
 
-type BaseMatrix = {
-  /**
-   * Path to the file/folder being handled.
-   */
-  path: string;
-  /**
-   * The computed cache key, used to restore files from the CI.
-   */
-  cacheKey: string;
-  /**
-   * The list of clients to run in the CI.
-   */
-  toRun: string;
-};
-
-export type ClientMatrix = BaseMatrix & {
-  /**
-   * The client language.
-   */
-  language: string;
-  /**
-   * The command to build the clients.
-   */
-  buildCommand: string;
-  /**
-   * The root of the test folder.
-   */
-  testsRootFolder: string;
-  /**
-   * The test output path to delete before running the CTS generation.
-   */
-  testsToDelete: string;
-  /**
-   * The test output path to store in the artifact.
-   */
-  testsToStore: string;
-  /**
-   * The snippets output path to store in the artifact.
-   */
-  snippetsToStore: string;
-};
-
-export type SpecMatrix = Pick<BaseMatrix, 'cacheKey' | 'toRun'> & {
-  /**
-   * The path of the bundled spec file.
-   */
-  bundledPath: string;
-};
-
-export type Matrix<TMatrix> = {
-  client: TMatrix[];
+export type ClientMatrix = {
+  client: Array<{
+    /**
+     * Path to the file/folder being handled.
+     */
+    path: string;
+    /**
+     * The list of clients to run in the CI.
+     */
+    toRun: string;
+    /**
+     * The client language.
+     */
+    language: string;
+    /**
+     * The command to build the clients.
+     */
+    buildCommand: string;
+    /**
+     * The root of the test folder.
+     */
+    testsRootFolder: string;
+    /**
+     * The test output path to delete before running the CTS generation.
+     */
+    testsToDelete: string;
+    /**
+     * The test output path to store in the artifact.
+     */
+    testsToStore: string;
+    /**
+     * The snippets output path to store in the artifact.
+     */
+    snippetsToStore: string;
+  }>;
 };
 
 export type ToRunMatrix = {
   path: string;
   toRun: string[];
-  cacheToCompute: string[];
 };
