4413b7d20a7df79eb082480160c5f5788fbd0cd0
Thomas Raffray <Fluf22@users.noreply.github.com>
2024-03-19 18:16:28 +0100

    fix(ci): run the swift cts on mac only on the main branch (#2888)

--- scripts/ci/githubActions/createMatrix.ts
+++ scripts/ci/githubActions/createMatrix.ts
@@ -16,7 +16,6 @@ import { isBaseChanged } from './utils.js';
 // This empty matrix is required by the CI, otherwise it throws
 const EMPTY_MATRIX = { client: ['no-run'] };
 
-// eslint-disable-next-line complexity
 async function createClientMatrix(baseBranch: string): Promise<void> {
   const matrix: Record<string, ToRunMatrix> = {};
   const commonDependenciesChanged =
@@ -142,9 +141,9 @@ async function createClientMatrix(baseBranch: string): Promise<void> {
     });
   }
 
-  // If the base branch is `main`, we also run the swift tests on macOS
+  // If there are updates for the Swift client, we allow ourselves to run the CTS on macOS
   const swiftData = clientMatrix.client.find((c) => c.language === 'swift');
-  if (swiftData && baseBranch === 'main') {
+  if (swiftData) {
     core.setOutput('SWIFT_DATA', JSON.stringify(swiftData));
     core.setOutput('RUN_MACOS_SWIFT_CTS', true);
   }
