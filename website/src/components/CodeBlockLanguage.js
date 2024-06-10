7cce384572fd3e16fddbc820584ebdef23c10565
Pierre Millot <pierre.millot@algolia.com>
2024-06-10 16:44:54 +0200

    try to have less params

--- website/src/components/CodeBlockLanguage.js
+++ website/src/components/CodeBlockLanguage.js
@@ -2,14 +2,21 @@ import React from 'react';
 import CodeBlock from '@theme/CodeBlock';
 import TabItem from '@theme/TabItem';
 import { languagesTabValues, TabsLanguage } from './TabsLanguage';
+import { addComment, getSnippet, waitForTaskSnippet } from '../snippets';
 
 export function CodeBlockLanguage(props) {
   return <TabsLanguage>
     {languagesTabValues.map((languageTabValue) => {
+      const language = languageTabValue.value;
       return (
-        <TabItem value={languageTabValue.value}>
-          <CodeBlock language={languageTabValue.value}>
-            {props.snippet(languageTabValue.value)}
+        <TabItem value={language}>
+          <CodeBlock language={language}>
+            {props.snippet({
+              getSnippet: (...args) => getSnippet(language, props.client || 'search', ...args),
+              addComment: (comment) => addComment(language, comment),
+              waitForTaskSnippet: () => waitForTaskSnippet(language),
+              language,
+            })}
           </CodeBlock>
         </TabItem>
       )
