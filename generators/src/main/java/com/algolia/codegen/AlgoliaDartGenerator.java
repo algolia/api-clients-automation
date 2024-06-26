91c64946d0987764d86410b216e97f53be6cd350
shortcuts <vannicattec@gmail.com>
2024-06-26 21:26:15 +0200

    fix(dart): add LICENSE to client_core

--- generators/src/main/java/com/algolia/codegen/AlgoliaDartGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaDartGenerator.java
@@ -100,6 +100,7 @@ public class AlgoliaDartGenerator extends DartDioClientCodegen {
 
     supportingFiles.add(new SupportingFile("version.mustache", srcFolder, "version.dart"));
     supportingFiles.add(new SupportingFile("LICENSE", "", "LICENSE"));
+    supportingFiles.add(new SupportingFile("LICENSE", "../client_core/", "LICENSE"));
 
     // Search config
     additionalProperties.put("isSearchClient", client.equals("search"));
