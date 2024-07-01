d812167a58d39acfc587d088f84710062038d5bf
Klemen Tušar <techouse@gmail.com>
2024-07-01 09:05:14 +0100

    feat(dart): add license generation

--- generators/src/main/java/com/algolia/codegen/AlgoliaDartGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaDartGenerator.java
@@ -101,6 +101,7 @@ public class AlgoliaDartGenerator extends DartDioClientCodegen {
     supportingFiles.add(new SupportingFile("version.mustache", srcFolder, "version.dart"));
     supportingFiles.add(new SupportingFile("LICENSE", "", "LICENSE"));
     supportingFiles.add(new SupportingFile("LICENSE", "../client_core/", "LICENSE"));
+    supportingFiles.add(new SupportingFile("LICENSE", "../chopper_requester/", "LICENSE"));
 
     // Search config
     additionalProperties.put("isSearchClient", client.equals("search"));
