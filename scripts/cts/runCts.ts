1d9973affeb2bc200cad31c9e9313eef462ae982
Pierre Millot <pierre.millot@algolia.com>
2024-07-09 19:04:12 +0200

    fix(cts): retry e2e tests

--- scripts/cts/runCts.ts
+++ scripts/cts/runCts.ts
@@ -7,24 +7,52 @@ import { assertChunkWrapperValid } from './testServer/chunkWrapper.js';
 import { assertValidReplaceAllObjects } from './testServer/replaceAllObjects.js';
 import { assertValidTimeouts } from './testServer/timeout.js';
 
-async function runCtsOne(language: string): Promise<void> {
-  const spinner = createSpinner(`running cts for '${language}'`);
+async function runCtsOne(
+  language: Language,
+  excludeE2E: boolean,
+  excludeUnit: boolean,
+): Promise<void> {
   const cwd = `tests/output/${language}`;
+
+  const folders: string[] = [];
+  if (language !== 'dart' && language !== 'kotlin' && !excludeE2E) {
+    folders.push('e2e');
+  }
+  if (!excludeUnit) {
+    folders.push('client', 'requests');
+  }
+
+  const spinner = createSpinner(`running cts for '${language}' in folder(s) ${folders.join(', ')}`);
+
+  if (folders.length === 0) {
+    spinner.succeed(`skipping '${language}' because all tests are excluded`);
+    return;
+  }
+
   switch (language) {
     case 'csharp':
       await run('dotnet test /clp:ErrorsOnly', { cwd, language });
       break;
     case 'dart':
-      await run('dart test', { cwd, language });
-      break;
-    case 'go':
-      await run(`go test -race -count 1 ${isVerbose() ? '-v' : ''} ./...`, {
+      await run(`dart test ${folders.map((f) => `test/${f}`).join(' ')}`, {
         cwd,
         language,
       });
       break;
+    case 'go':
+      await run(
+        `go test -race -count 1 ${isVerbose() ? '-v' : ''} ${folders.map((f) => `gotests/tests/${f}/...`).join(' ')}`,
+        {
+          cwd,
+          language,
+        },
+      );
+      break;
     case 'java':
-      await run('./gradle/gradlew -p tests/output/java test --rerun', { language });
+      await run(
+        `./gradle/gradlew -p tests/output/java test --rerun ${folders.map((f) => `--tests 'com.algolia.${f}*'`).join(' ')}`,
+        { language },
+      );
       break;
     case 'javascript':
       await run('YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install && yarn test', {
@@ -72,14 +100,19 @@ async function runCtsOne(language: string): Promise<void> {
 }
 
 // the clients option is only used to determine if we need to start the test server, it will run the tests for all clients anyway.
-export async function runCts(languages: Language[], clients: string[]): Promise<void> {
-  const useTestServer = clients.includes('search') || clients.includes('all');
+export async function runCts(
+  languages: Language[],
+  clients: string[],
+  excludeE2E: boolean,
+  excludeUnit: boolean,
+): Promise<void> {
+  const useTestServer = !excludeUnit && (clients.includes('search') || clients.includes('all'));
   let close: () => Promise<void> = async () => {};
   if (useTestServer) {
     close = await startTestServer();
   }
   for (const lang of languages) {
-    await runCtsOne(lang);
+    await runCtsOne(lang, excludeE2E, excludeUnit);
   }
 
   if (useTestServer) {
