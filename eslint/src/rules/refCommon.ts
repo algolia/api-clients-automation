671b94560bdcccf031c3beb7d1cf7fcc78d4f6d9
Pierre Millot <pierre.millot@algolia.com>
2024-07-24 14:21:57 +0200

    chore(specs): add eslint rule to avoid cross-references

---
+++ eslint/src/rules/refCommon.ts
@@ -0,0 +1,72 @@
+import type { Rule } from 'eslint';
+
+import { isPairWithKey, isScalar } from '../utils';
+
+const allSpecs = [
+  'abtesting',
+  'analytics',
+  'crawler',
+  'ingestion',
+  'insights',
+  'monitoring',
+  'personalization',
+  'query-suggestions',
+  'recommend',
+  'search',
+  'usage',
+];
+
+export const refCommon: Rule.RuleModule = {
+  meta: {
+    docs: {
+      description: 'the $ref must target the current spec, or the common spec',
+    },
+    messages: {
+      refCommon: '$ref to another spec',
+    },
+  },
+  create(context) {
+    if (!context.sourceCode.parserServices.isYAML) {
+      return {};
+    }
+
+    return {
+      YAMLPair(node): void {
+        if (!isPairWithKey(node, '$ref')) {
+          return;
+        }
+
+        if (!isScalar(node.value)) {
+          return;
+        }
+
+        let ref = node.value.value;
+        if (
+          typeof ref !== 'string' ||
+          ref.trim().startsWith('#/') ||
+          ref.startsWith('./')
+        ) {
+          return;
+        }
+
+        const spec = context.filename.match(/specs\/([a-z-]+?)\//)?.[1];
+        if (!spec) {
+          return;
+        }
+        while (ref.startsWith(`../`)) {
+          ref = ref.slice(3);
+        }
+        if (
+          allSpecs.filter((s) => s !== spec).every((s) => !ref.startsWith(s))
+        ) {
+          return;
+        }
+
+        context.report({
+          node: node as any,
+          messageId: 'refCommon',
+        });
+      },
+    };
+  },
+};
--- eslint/tests/outOfLineRule.test.ts
+++ eslint/tests/outOfLineRule.test.ts
@@ -66,16 +66,19 @@ useIt:
   ],
 });
 
-
 // oneOf should allow `type: 'null'`
-ruleTester.run('out-of-line-oneOf-null', createOutOfLineRule({ property: 'oneOf' }), {
-  valid: [
-    `
+ruleTester.run(
+  'out-of-line-oneOf-null',
+  createOutOfLineRule({ property: 'oneOf' }),
+  {
+    valid: [
+      `
 simple:
   oneOf:
     - type: string
     - type: 'null'
-`,`
+`,
+      `
 obj:
   type: object
   properties:
@@ -83,10 +86,11 @@ obj:
       oneOf:
         - type: string
         - type: 'null'
-`],
-  invalid: [
-    {
-      code: `
+`,
+    ],
+    invalid: [
+      {
+        code: `
 simple:
   type: object
   properties:
@@ -95,21 +99,26 @@ simple:
         - type: string
         - type: null
       `,
-      errors: [{ messageId: 'oneOfNotOutOfLine' }],
-    },
-  ],
-});
+        errors: [{ messageId: 'oneOfNotOutOfLine' }],
+      },
+    ],
+  }
+);
 
 // allow enum to be nullable
-ruleTester.run('out-of-line-enum-null', createOutOfLineRule({ property: 'enum' }), {
-  valid: [
-    `
+ruleTester.run(
+  'out-of-line-enum-null',
+  createOutOfLineRule({ property: 'enum' }),
+  {
+    valid: [
+      `
 simple:
   oneOf:
     - type: string
       enum: [bla, blabla]
     - type: 'null'
-`],
-  invalid: [],
-});
-
+`,
+    ],
+    invalid: [],
+  }
+);
