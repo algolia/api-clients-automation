3f59ba4b218926a7999439112698d16f10c3b329
shortcuts <vannicattec@gmail.com>
2024-08-06 13:27:12 +0200

    chore: new logic part 2

--- scripts/release/sla.ts
+++ scripts/release/sla.ts
@@ -20,22 +20,30 @@ function getMajorMinor(lang: Language, version: string): { major: number; minor:
   return { major: parseInt(matches[1], 10), minor: parseInt(matches[2], 10) };
 }
 
+// isPreRelease
 function isPreRelease(version: string): boolean {
   return (
     version.match(preReleaseRegExp) !== null ||
     semver.prerelease(version) !== null ||
     version.includes('beta') ||
-    version.includes('alpha')
+    version.includes('alpha') ||
+    version.startsWith('0') ||
+    isNaN(parseInt(version.charAt(0), 10))
   );
 }
 
-// updates the release.config.json in order to generate a map of version and SLA for every clients
-export async function generateLanguageSLA(lang: Language, versions: Versions): Promise<void> {
-  const githubToken = ensureGitHubToken();
-  const start = new Date();
-  const end = new Date(new Date().setFullYear(start.getFullYear() + 2));
+function setSupportStatus(lang: Language, version: string, status: 'active' | 'inactive'): void {
+  if (status === 'inactive') {
+    delete fullReleaseConfig.sla[lang][version].supportStart;
+    delete fullReleaseConfig.sla[lang][version].supportEnd;
+  }
 
-  console.log(`> generating SLA for the ${lang} client`);
+  fullReleaseConfig.sla[lang][version].supportStatus = status;
+}
+
+// fetch the git tags for the given `lang`
+async function getTags(lang: Language): Promise<string[]> {
+  const githubToken = ensureGitHubToken();
 
   // fetch all release tags for this lang
   const { tempGitDir } = await cloneRepository({
@@ -56,22 +64,30 @@ export async function generateLanguageSLA(lang: Language, versions: Versions): P
     throw new Error(`unable to find tags for language ${lang}`);
   }
 
+  return tags;
+}
+
+// updates the release.config.json in order to generate a map of version and SLA for every clients
+export function generateLanguageSLA(tags: string[], lang: Language, versions: Versions): void {
+  const start = new Date();
+
   // @ts-expect-error -- force reset our sla policy to remove outdated ones
   fullReleaseConfig.sla[lang] = {};
 
   let prevTagMajor = 0;
   let prevTagMinor = 0;
-  let activeVersion = '';
+  let prevTagVersion = '';
 
   for (const tag of tags) {
     // eslint-disable-next-line prefer-const
     let [tagVersion, tagReleaseDate] = tag.split(/(?<=^\S+)\s/);
 
+    // edge case for go which tags version with a v prefix
     if (tagVersion.startsWith('v')) {
       tagVersion = tagVersion.replace(/^v/, '');
     }
 
-    // we skip any form of pre-releases
+    // we skip any form of pre-releases because they are not part of the SLA
     if (isPreRelease(tagVersion)) {
       continue;
     }
@@ -80,53 +96,63 @@ export async function generateLanguageSLA(lang: Language, versions: Versions): P
     const deadline = new Date(releaseDate);
     deadline.setFullYear(releaseDate.getFullYear() + 2);
 
-    // if it's older than today, it's not supported anymore
     if (start.getTime() > deadline.getTime()) {
       continue;
     }
 
     const { major: tagMajor, minor: tagMinor } = getMajorMinor(lang, tagVersion);
 
-    // we only want the last patch of the current major.minor
-    if (tagMajor === prevTagMajor && tagMinor === prevTagMinor) {
-      delete fullReleaseConfig.sla[lang][activeVersion];
+    fullReleaseConfig.sla[lang][tagVersion] = {
+      releaseDate: releaseDate.toISOString().split('T')[0],
+    };
+
+    // the current tag version defines the maintenance policy of the previous one
+    if (prevTagVersion !== '') {
+      fullReleaseConfig.sla[lang][prevTagVersion] = {
+        ...fullReleaseConfig.sla[lang][prevTagVersion],
+        supportStart: releaseDate.toISOString().split('T')[0],
+        supportEnd: deadline.toISOString().split('T')[0],
+        supportStatus: 'maintenance',
+      };
+
+      // if the previous tag is on the same major.minor version, we don't support it
+      if (tagMajor === prevTagMajor && tagMinor === prevTagMinor) {
+        setSupportStatus(lang, prevTagVersion, 'inactive');
+      }
     }
 
-    fullReleaseConfig.sla[lang][tagVersion] = {
-      start: releaseDate.toISOString().split('T')[0],
-      end: deadline.toISOString().split('T')[0],
-      status: 'maintenance',
-    };
     prevTagMajor = tagMajor;
     prevTagMinor = tagMinor;
-    activeVersion = tagVersion;
+    prevTagVersion = tagVersion;
   }
 
   // if there's no release planned, or if the release is a pre-release, then the latest tagged version is the active one
   if (!versions[lang]?.next || isPreRelease(versions[lang]?.next)) {
-    fullReleaseConfig.sla[lang][activeVersion].status = 'active';
-
-    return;
+    return setSupportStatus(lang, prevTagVersion, 'active');
   }
 
   const { major: nextMajor, minor: nextMinor } = getMajorMinor(lang, versions[lang].next);
 
-  // if we are releasing the same major.minor version, we erase the current and only provide SLA for the latest patch
+  // if we release a new patch on the same major.minor version as the active one, we set it as inactive
   if (nextMajor === prevTagMajor && nextMinor === prevTagMinor) {
-    delete fullReleaseConfig.sla[lang][activeVersion];
+    return setSupportStatus(lang, prevTagVersion, 'inactive');
   }
 
-  // if it's a new major or minor, it's just a new active version
-  fullReleaseConfig.sla[lang][versions[lang].next] = {
-    start: start.toISOString().split('T')[0],
-    end: end.toISOString().split('T')[0],
-    status: 'active',
-  };
+  // if it's a new major or minor, it's just a new active version, the previous one is already in maintenance
+  return setSupportStatus(lang, versions[lang].next, 'active');
 }
 
 export async function generateSLA(versions: Versions): Promise<void> {
-  await Promise.all(LANGUAGES.map((lang) => generateLanguageSLA(lang, versions)));
-  // generateLanguageSLA('javascript', versions);
+  await Promise.all(
+    LANGUAGES.map(async (lang) => {
+      console.log(`> generating SLA for the ${lang} client`);
+
+      const tags = await getTags(lang);
+
+      return generateLanguageSLA(tags, lang, versions);
+    }),
+  );
+  // await generateLanguageSLA('csharp', versions);
 
   await fsp.writeFile(
     toAbsolutePath('config/release.config.json'),
