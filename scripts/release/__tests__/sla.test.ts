d61119a0082d9579e867ab6ff71c6e5c4be8a568
Clément Vannicatte <vannicattec@gmail.com>
2024-08-14 09:54:37 +0200

    chore: skip SLA generation (#3519)

--- scripts/release/__tests__/sla.test.ts
+++ scripts/release/__tests__/sla.test.ts
@@ -1,8 +1,17 @@
 import {  describe, expect, it } from "vitest";
 import { generateLanguageSLA } from '../sla.js';
-import { fullReleaseConfig } from '../../common.js';
+import { fullReleaseConfig, LANGUAGES } from '../../common.js';
 
 describe('generateLanguageSLA', () => {
+  LANGUAGES.forEach((lang) => {
+    if (!('sla' in fullReleaseConfig)) {
+      fullReleaseConfig['sla'] = {}
+    }
+
+    //@ts-expect-error
+    fullReleaseConfig.sla[lang] = {}
+  })
+
   describe("no new releases", () => {
     it('parses version of the same minor', () => {
       generateLanguageSLA([
@@ -12,6 +21,7 @@ describe('generateLanguageSLA', () => {
         "1.2.7 Thu Jan 4 15:09:11 2024 +0000",
       ], 'csharp', {current: "1.2.7", next: null, releaseType: null})
 
+      //@ts-expect-error
       expect(fullReleaseConfig.sla.csharp).toEqual({
         "1.2.4": {
           "releaseDate": "2023-12-28",
@@ -43,6 +53,7 @@ describe('generateLanguageSLA', () => {
         "1.4.7 Thu Jan 4 15:09:11 2024 +0000",
       ], 'javascript', {current: "1.4.7", next: null, releaseType: null})
 
+      //@ts-expect-error
       expect(fullReleaseConfig.sla.javascript).toEqual({
         "1.1.4": {
           "releaseDate": "2023-12-28",
@@ -76,6 +87,7 @@ describe('generateLanguageSLA', () => {
         "v1.4.7 Tue Jan 2 14:17:11 2024 +0000",
       ], 'php', {current: "1.4.7", next: null, releaseType: null})
 
+      //@ts-expect-error
       expect(fullReleaseConfig.sla.php).toEqual({
         "1.1.4": {
           "releaseDate": "2021-12-28",
@@ -99,6 +111,7 @@ describe('generateLanguageSLA', () => {
         "v1.4.7 Tue Jan 2 14:17:11 2024 +0000",
       ], 'go', {current: "1.4.7", next: null, releaseType: null})
 
+      //@ts-expect-error
       expect(fullReleaseConfig.sla.go).toEqual({
         "1.1.4": {
           "releaseDate": "2023-12-28",
@@ -124,6 +137,7 @@ describe('generateLanguageSLA', () => {
         "1.2.4 Thu Dec 28 15:48:25 2023 +0000",
       ], 'dart', {next: "1.2.4", current: "1.2.4", releaseType: "minor"})
 
+      //@ts-expect-error
       expect(fullReleaseConfig.sla.dart).toEqual({
         "1.2.4": {
           "releaseDate": "2023-12-28",
@@ -137,6 +151,7 @@ describe('generateLanguageSLA', () => {
         "1.2.4 Thu Dec 28 15:48:25 2023 +0000",
       ], 'dart', {next: "2.0.0", current: "1.2.4", releaseType: "major"})
 
+      //@ts-expect-error
       expect(fullReleaseConfig.sla.dart).toEqual({
         "1.2.4": {
           "releaseDate": "2023-12-28",
@@ -156,6 +171,7 @@ describe('generateLanguageSLA', () => {
         "1.2.4 Thu Dec 28 15:48:25 2023 +0000",
       ], 'kotlin', {next: "1.3.0", current: "1.2.4", releaseType: "minor"})
 
+      //@ts-expect-error
       expect(fullReleaseConfig.sla.kotlin).toEqual({
         "1.2.4": {
           "releaseDate": "2023-12-28",
@@ -175,6 +191,7 @@ describe('generateLanguageSLA', () => {
         "1.2.4 Thu Dec 28 15:48:25 2023 +0000",
       ], 'swift', {next: "1.2.5", current: "1.2.4", releaseType: "patch"})
 
+      //@ts-expect-error
       expect(fullReleaseConfig.sla.swift).toEqual({
         "1.2.4": {
           "releaseDate": "2023-12-28",
