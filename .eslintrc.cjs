b4d8a8d758de606646578cb9006de34917654d46
shortcuts <vannicattec@gmail.com>
2024-08-19 11:21:18 +0200

    chore: upgrade node

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -127,7 +127,7 @@ module.exports = {
             ],
           },
         ],
-        '@typescript-eslint/ban-types': [
+        '@typescript-eslint/no-restricted-types': [
           'error',
           {
             types: {
