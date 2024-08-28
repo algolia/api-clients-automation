180710dafd9007dd9790e8f3efc672db5f958866
Pierre Millot <pierre.millot@algolia.com>
2024-08-28 15:52:45 +0200

    chore: add a bug report template to github

--- generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
@@ -61,6 +61,7 @@ public class AlgoliaJavascriptGenerator extends TypeScriptNodeClientCodegen {
 
     supportingFiles.add(new SupportingFile("LICENSE", "", "LICENSE"));
     supportingFiles.add(new SupportingFile("issue.yml", "../../.github/workflows", "issue.yml"));
+    supportingFiles.add(new SupportingFile("Bug_report.yml", "../../.github/ISSUE_TEMPLATE", "Bug_report.yml"));
 
     supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
 
