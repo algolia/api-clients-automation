9f0ff6519b452feee878a8259790685d29459a80
Pierre Millot <pierre.millot@algolia.com>
2024-08-28 12:04:05 +0200

    chore(snippets): build

--- scripts/buildClients.ts
+++ scripts/buildClients.ts
@@ -3,19 +3,21 @@ import { getLanguageFolder } from './config.js';
 import { createSpinner } from './spinners.js';
 import type { Generator, Language } from './types.js';
 
+type BuildType = 'client' | 'playground' | 'snippets';
+
 /**
  * Build code for a specific language.
  */
-async function buildLanguage(language: Language, gens: Generator[], playground: boolean): Promise<void> {
-  const cwd = playground ? `playground/${language}` : getLanguageFolder(language);
-  const spinner = createSpinner(`building ${playground ? 'playground' : 'client'} '${language}'`);
+async function buildLanguage(language: Language, gens: Generator[], buildType: BuildType): Promise<void> {
+  const cwd = buildType === 'client' ? getLanguageFolder(language) : `./${buildType}/${language}`;
+  const spinner = createSpinner(`building ${buildType} for '${language}'`);
   switch (language) {
     case 'csharp':
       await run('dotnet build --configuration Release', { cwd, language });
       break;
     case 'javascript':
       await run('YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install', { cwd });
-      if (!playground) {
+      if (buildType === 'client') {
         const packageNames = gens.map(({ additionalProperties: { packageName } }) =>
           packageName === 'algoliasearch' ? packageName : `@algolia/${packageName}`,
         );
@@ -35,7 +37,7 @@ async function buildLanguage(language: Language, gens: Generator[], playground: 
       break;
     case 'swift':
       // make this work in the playground
-      if (!playground) {
+      if (buildType === 'client') {
         await run('swift build -Xswiftc -suppress-warnings', { cwd, language });
       }
       break;
@@ -59,9 +61,13 @@ export async function buildClients(generators: Generator[]): Promise<void> {
     {} as Record<Language, Generator[]>,
   );
 
-  await Promise.all(langs.map((lang) => buildLanguage(lang, generatorsMap[lang], false)));
+  await Promise.all(langs.map((lang) => buildLanguage(lang, generatorsMap[lang], 'client')));
 }
 
 export async function buildPlaygrounds(languages: Language[]): Promise<void> {
-  await Promise.all(languages.map((lang) => buildLanguage(lang, [], true)));
+  await Promise.all(languages.map((lang) => buildLanguage(lang, [], 'playground')));
+}
+
+export async function buildSnippets(languages: Language[]): Promise<void> {
+  await Promise.all(languages.map((lang) => buildLanguage(lang, [], 'snippets')));
 }
