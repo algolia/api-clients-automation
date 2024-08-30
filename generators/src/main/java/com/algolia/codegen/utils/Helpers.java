c0675ff6bf4b7d52106775c88cf8af6a203c06a0
Clément Vannicatte <vannicattec@gmail.com>
2024-08-30 14:59:06 +0200

    fix(clients): correly parse usage hosts (#3622)

--- generators/src/main/java/com/algolia/codegen/utils/Helpers.java
+++ generators/src/main/java/com/algolia/codegen/utils/Helpers.java
@@ -92,6 +92,7 @@ public class Helpers {
     try {
       boolean hasRegionalHost = false;
       boolean fallbackToAliasHost = false;
+      boolean hasVariables = false;
       String regionalHost = "";
       String hostWithFallback = "";
       Set<String> allowedRegions = new HashSet<>();
@@ -117,6 +118,7 @@ public class Helpers {
         if (server.variables == null || server.variables.isEmpty()) {
           continue;
         }
+        hasVariables = true;
         CodegenServerVariable regionVar = server.variables.stream().filter(v -> v.name.equals("region")).findFirst().orElse(null);
         if (regionVar == null || regionVar.enumValues == null || regionVar.enumValues.isEmpty()) {
           continue;
@@ -133,9 +135,13 @@ public class Helpers {
       }
 
       if (!hasRegionalHost) {
-        if (servers.size() == 1 && hostWithFallback.isEmpty()) {
-          URL url = new URL(servers.get(0).url);
-          bundle.put("uniqueHost", url.getHost());
+        if (!hasVariables && hostWithFallback.isEmpty()) {
+          List<String> hostsWithoutVariables = new ArrayList<>();
+          for (CodegenServer otherServer : servers) {
+            URL url = new URL(otherServer.url);
+            hostsWithoutVariables.add(url.getHost());
+          }
+          bundle.put("hostsWithoutVariables", hostsWithoutVariables);
         } else {
           bundle.put("hostWithAppID", true);
         }
