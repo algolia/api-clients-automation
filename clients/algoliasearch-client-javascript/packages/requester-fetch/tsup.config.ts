8f81c9466aa98cfee8706e16f2376bed9584e37b
shortcuts <vannicattec@gmail.com>
2024-09-01 23:30:26 +0200

    feat: node and browser for fetch requester

--- clients/algoliasearch-client-javascript/packages/requester-fetch/tsup.config.ts
+++ clients/algoliasearch-client-javascript/packages/requester-fetch/tsup.config.ts
@@ -3,19 +3,48 @@ import path from 'path';
 import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 
-const baseBrowserOptions: Options = {
+const baseConfig: Options = {
   clean: true,
   sourcemap: true,
   splitting: false,
   tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+};
+
+const baseNodeOptions: Options = {
+  ...baseConfig,
+  platform: 'node',
+  target: 'node14',
+  external: ['@algolia/client-common'],
+  entry: { 'requester.fetch.node': 'index.ts' },
+  dts: { entry: { 'requester.fetch.node': 'index.ts' } },
+};
+
+const baseBrowserOptions: Options = {
+  ...baseConfig,
+  name: 'browser esm',
   platform: 'browser',
   minify: true,
   format: ['esm'],
   target: ['chrome109', 'safari15.6', 'firefox115', 'edge126'],
-  globalName: 'requesterfetch',
-  entry: { 'requester.fetch': 'index.ts' },
-  dts: { entry: { 'requester.fetch': 'index.ts' } },
   external: ['dom', '@algolia/client-common'],
+  entry: { 'requester.fetch.browser': 'index.ts' },
+  dts: { entry: { 'requester.fetch.browser': 'index.ts' } },
+  globalName: 'requesterfetch',
 };
 
-export default defineConfig(baseBrowserOptions);
+const nodeConfigs: Options[] = [
+  {
+    ...baseNodeOptions,
+    format: 'cjs',
+    name: 'node cjs',
+  },
+  {
+    ...baseNodeOptions,
+    format: 'esm',
+    name: 'node esm',
+  },
+];
+
+const browserConfigs: Options[] = [baseBrowserOptions];
+
+export default defineConfig([...nodeConfigs, ...browserConfigs]);
