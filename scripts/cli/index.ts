fc1ba57fd1cfdb7704884e8df409cc4ea7fec5a9
Pierre Millot <pierre.millot@algolia.com>
2024-09-04 15:06:43 +0200

    revert

--- scripts/cli/index.ts
+++ scripts/cli/index.ts
@@ -2,7 +2,7 @@ import { Argument, program } from 'commander';
 import semver from 'semver';
 
 import { buildClients, buildPlaygrounds, buildSnippets } from '../buildClients.js';
-import { CLIENTS, LANGUAGES, run, setVerbose } from '../common.js';
+import { CI, CLIENTS, LANGUAGES, run, setVerbose } from '../common.js';
 import { ctsGenerateMany } from '../cts/generate.js';
 import { runCts } from '../cts/runCts.js';
 import { startTestServer } from '../cts/testServer';
@@ -41,8 +41,10 @@ const flags = {
 program.name('cli');
 
 program.hook('preAction', () => {
-  // restore the cursor because sometime it's broken
-  process.stdout.write('\x1B[?25h');
+  if (!CI) {
+    // restore the cursor because sometime it's broken
+    process.stdout.write('\x1B[?25h');
+  }
 });
 
 program
