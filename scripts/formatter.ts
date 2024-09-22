7cde7bee0c727d265aa7aa0df5e4d05f535aead2
Pierre Millot <pierre.millot@algolia.com>
2024-09-22 22:02:03 +0200

    fix(go): true speed of benchmark (#3786)

--- scripts/formatter.ts
+++ scripts/formatter.ts
@@ -37,7 +37,7 @@ export async function formatter(language: string, cwd: string): Promise<void> {
       );
       break;
     case 'javascript':
-      await run(`yarn eslint --ext=ts,json ${cwd} --fix --no-error-on-unmatched-pattern`);
+      await run(`yarn eslint --ext=ts,json ${cwd} --fix --no-error-on-unmatched-pattern`, { language });
       break;
     case 'kotlin':
       await run(`./gradle/gradlew -p ${cwd} spotlessApply`, { language });
