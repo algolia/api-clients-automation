0179bdbfd1c784b937f84b9080401bf5ce641fc6
Pierre Millot <pierre.millot@algolia.com>
2024-09-23 17:00:18 +0200

    feat(clients): cleanup after replaceAllObjects failure

--- scripts/cts/runCts.ts
+++ scripts/cts/runCts.ts
@@ -9,6 +9,7 @@ import { printBenchmarkReport } from './testServer/benchmark.js';
 import { assertChunkWrapperValid } from './testServer/chunkWrapper.js';
 import { startTestServer } from './testServer/index.js';
 import { assertValidReplaceAllObjects } from './testServer/replaceAllObjects.js';
+import { assertValidReplaceAllObjectsFailed } from './testServer/replaceAllObjectsFailed.js';
 import { assertValidTimeouts } from './testServer/timeout.js';
 import { assertValidWaitForApiKey } from './testServer/waitFor.js';
 
@@ -152,6 +153,7 @@ export async function runCts(
     assertValidTimeouts(languages.length);
     assertChunkWrapperValid(languages.length - skip('dart') - skip('scala'));
     assertValidReplaceAllObjects(languages.length - skip('dart') - skip('scala'));
+    assertValidReplaceAllObjectsFailed(languages.length - skip('dart') - skip('scala'));
     assertValidWaitForApiKey(languages.length - skip('dart') - skip('scala'));
   }
   if (withBenchmarkServer) {
