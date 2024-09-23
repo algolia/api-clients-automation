bcfd0d1d27d57d7006fabfb880da3eaefee71d15
shortcuts <vannicattec@gmail.com>
2024-09-23 12:23:25 +0200

    fix(tests): assert global to_dict rather than model-level one

--- tests/output/python/tests/helpers.py
+++ tests/output/python/tests/helpers.py
@@ -1,4 +1,13 @@
 class Helpers:
+    def unwrap(self, resp):
+        """ converts the object response to its dictionary form, if it's a list, every items are iterated on and we call to_dict on it, otherwise we just call to_dict """
+        if isinstance(resp, list):
+            _res = []
+            for _, r in enumerate(resp):
+                _res.append(r.to_dict())
+            return _res
+        return resp.to_dict()
+
     def union(self, expected, received):
         """
         creates a new result based on the keys of 'expected' that are also in 'received'
@@ -9,8 +18,6 @@ class Helpers:
                 _res.append(self.union(_v, received[_iv]))
             return _res
         if isinstance(expected, dict):
-            if hasattr(received, "to_dict"):
-                received = received.to_dict()
             _res = {}
             for k, v in expected.items():
                 if k not in received or received[k] is None:
