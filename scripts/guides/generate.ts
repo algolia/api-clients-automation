1f69a015a8f28295a5f48b4049878a0986ce40e8
shortcuts <vannicattec@gmail.com>
2024-09-26 15:06:33 +0200

    fix: run only on existing guides

--- scripts/guides/generate.ts
+++ scripts/guides/generate.ts
@@ -1,9 +1,13 @@
-import { callGenerator, exists, run, setupAndGen, toAbsolutePath } from '../common.js';
+import { existsSync } from 'node:fs';
+
+import { callGenerator, run, setupAndGen, toAbsolutePath } from '../common.js';
 import { getTestOutputFolder } from '../config.js';
 import { formatter } from '../formatter.js';
 import type { Generator } from '../types.js';
 
 export async function guidesGenerateMany(generators: Generator[]): Promise<void> {
+  generators = generators.filter((gen) => existsSync(toAbsolutePath(`templates/${gen.language}/guides/${gen.client}`)));
+
   await setupAndGen(generators, 'guides', async (gen) => {
     if (getTestOutputFolder(gen.language)) {
       await callGenerator(gen);
@@ -12,6 +16,7 @@ export async function guidesGenerateMany(generators: Generator[]): Promise<void>
 
   const langs = [...new Set(generators.map((gen) => gen.language))];
   for (const lang of langs) {
+    const guidesPath = `guides/${lang}`;
     if (!getTestOutputFolder(lang)) {
       continue;
     }
@@ -29,9 +34,6 @@ export async function guidesGenerateMany(generators: Generator[]): Promise<void>
       });
     }
 
-    const guidesPath = `guides/${lang}`;
-    if (await exists(toAbsolutePath(guidesPath))) {
-      await formatter(lang, guidesPath);
-    }
+    await formatter(lang, guidesPath);
   }
 }
