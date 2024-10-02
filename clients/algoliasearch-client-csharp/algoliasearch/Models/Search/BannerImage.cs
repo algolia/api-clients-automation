82b04e57be9356263eaa539236b410ceafbd84ae
Jonas Kalmar Rønning <jonaskalmar.ronning@komplett.com>
2024-10-02 14:27:47 +0200

    feat(csharp) add support for widgets / banners in search for the csharp client fix #3869

---
+++ clients/algoliasearch-client-csharp/algoliasearch/Models/Search/BannerImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Algolia.Search.Models.Search;
+
+/// <summary>
+/// A banner image, contains urls for the images and a title
+/// </summary>
+public class BannerImage
+{
+  /// <summary>
+  /// Gets or sets urls
+  /// </summary>
+  [JsonPropertyName("urls")]
+  public IReadOnlyCollection<BannerImageUrl> Urls { get; set; }
+
+  /// <summary>
+  /// Gets or sets title
+  /// </summary>
+  [JsonPropertyName("title")]
+  public string Title { get; set; }
+}
---
+++ clients/algoliasearch-client-csharp/algoliasearch/Models/Search/Widgets.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Algolia.Search.Models.Search;
+
+public class Widgets
+{
+  /// <summary>
+  /// Gets or sets banners
+  /// </summary>
+  [JsonPropertyName("banners")]
+  public IReadOnlyCollection<Banner> Banners { get; set; }
+}
