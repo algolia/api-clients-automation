e64684b29e2c167ed0ccf6f795174f269aaa60e8
Pierre Millot <pierre.millot@algolia.com>
2024-10-03 12:03:26 +0200

    no type checking in tests

--- scripts/formatter.ts
+++ scripts/formatter.ts
@@ -51,9 +51,12 @@ export async function formatter(language: string, cwd: string): Promise<void> {
       break;
     case 'python':
       await run(
-        `poetry lock --no-update && poetry install --sync && pip freeze > requirements.txt && poetry run ruff check --fix --unsafe-fixes && poetry run ruff format${cwd.includes('clients') ? ' && poetry run pyright' : ''}`,
+        'poetry lock --no-update && poetry install --sync && pip freeze > requirements.txt && poetry run ruff check --fix --unsafe-fixes && poetry run ruff format',
         { cwd, language },
       );
+      if (!cwd.includes('tests')) {
+        await run('poetry run pyright', { cwd, language });
+      }
       break;
     case 'ruby':
       await run('bundle install', { cwd, language });
