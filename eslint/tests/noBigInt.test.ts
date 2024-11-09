986b7644114c442fc53eac56d285f27e8cc80e1c
Pierre Millot <pierre.millot@algolia.com>
2024-11-09 19:13:32 +0100

    chore(eslint): add linter to forbid bigInt

---
+++ eslint/tests/noBigInt.test.ts
@@ -0,0 +1,51 @@
+import { runClassic } from 'eslint-vitest-rule-tester';
+import yamlParser from 'yaml-eslint-parser';
+import { noBigInt } from '../src/rules/noBigInt.js';
+
+
+runClassic(
+  'no-big-int',
+  noBigInt,
+  {
+    valid: [`
+type: object
+properties:
+  id:
+    type: number
+    format: float
+  url:
+    type: string
+    format: uri
+`, `
+prop:
+  type: integer
+  format: int32
+`],
+    invalid: [
+      {
+        code: `
+type: object
+properties:
+  id:
+    type: number
+    format: int64
+  url:
+    type: string
+    format: uri
+`,
+        errors: [{ messageId: 'noBigInt' }],
+      },
+      {
+        code: `
+prop:
+  type: number
+  format: int32
+`,
+        errors: [{ messageId: 'noBigInt' }],
+      },
+    ],
+  },
+  {
+    parser: yamlParser,
+  },
+);
