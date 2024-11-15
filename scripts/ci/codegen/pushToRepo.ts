4e354c544f676eecf5cf2269be1252b2ca33d198
Levi Whalen <levi@leviwhalen.com>
2024-11-15 15:58:22 -0700

    uncomment

--- scripts/ci/codegen/pushToRepo.ts
+++ scripts/ci/codegen/pushToRepo.ts
@@ -1,7 +1,20 @@
 import fsp from 'fs/promises';
 import { resolve } from 'path';
 
-import { ensureGitHubToken, exists, LANGUAGES, OWNER, run, setVerbose, toAbsolutePath } from '../../common.js';
+import {
+  configureGitHubAuthor,
+  ensureGitHubToken,
+  exists,
+  getOctokit,
+  gitBranchExists,
+  gitCommit,
+  LANGUAGES,
+  OWNER,
+  run,
+  setVerbose,
+  toAbsolutePath,
+} from '../../common.js';
+import { getNbGitDiff } from '../utils.js';
 import { pushConfigs, type PushConfig } from './pushConfigs.js';
 
 import { getClientsConfigField } from '../../config.js';
@@ -18,7 +31,6 @@ async function generateJSON(outputFile: string, guideNames: PushConfig['guides']
       `docs/guides/${language}/${getClientsConfigField(language, ['snippets', 'outputFolder'])}`,
     );
     const files = await fsp.readdir(pathToGuides);
-
     for (const file of files.filter((file) => guideNames.some((guideName) => guideName === file.split('.')[0]))) {
       const extension = getClientsConfigField(language, ['snippets', 'extension']);
       if (!file.endsWith(extension)) {
@@ -37,7 +49,6 @@ async function generateJSON(outputFile: string, guideNames: PushConfig['guides']
     }
   }
 
-  console.log(guides);
   await fsp.writeFile(outputFile, JSON.stringify(guides, null, 2));
 }
 
@@ -46,11 +57,17 @@ async function pushToRepo(config: PushConfig): Promise<void> {
 
   const repository = config.destination.repo;
   const lastCommitMessage = await run('git log -1 --format="%s"');
+  const author = (await run('git log -1 --format="Co-authored-by: %an <%ae>"')).trim();
+  const coAuthors = (await run('git log -1 --format="%(trailers:key=Co-authored-by)"'))
+    .split('\n')
+    .map((coAuthor) => coAuthor.trim())
+    .filter(Boolean);
 
   if (!process.env.FORCE && !lastCommitMessage.startsWith(commitStartRelease)) {
     return;
   }
 
+  const targetBranch = config.destination.prBranch;
   const githubURL = `https://${githubToken}:${githubToken}@github.com/${OWNER}/${repository}`;
   const tempGitDir = resolve(process.env.RUNNER_TEMP! || toAbsolutePath('foo/local/test'), repository);
   await fsp.rm(tempGitDir, { force: true, recursive: true });
@@ -62,45 +79,45 @@ async function pushToRepo(config: PushConfig): Promise<void> {
 
   await generateJSON(toAbsolutePath(`${tempGitDir}/${config.outputFilePath}`), config.guides);
 
-  // console.log(`Pushing to ${OWNER}/${repository}`);
-  // if (await gitBranchExists(targetBranch, tempGitDir)) {
-  //   await run(`git fetch origin ${targetBranch}`, { cwd: tempGitDir });
-  //   await run(`git push -d origin ${targetBranch}`, { cwd: tempGitDir });
-  // }
-  // await run(`git checkout -B ${targetBranch}`, { cwd: tempGitDir });
+  console.log(`Pushing to ${OWNER}/${repository}`);
+  if (await gitBranchExists(targetBranch, tempGitDir)) {
+    await run(`git fetch origin ${targetBranch}`, { cwd: tempGitDir });
+    await run(`git push -d origin ${targetBranch}`, { cwd: tempGitDir });
+  }
+  await run(`git checkout -B ${targetBranch}`, { cwd: tempGitDir });
 
-  // if ((await getNbGitDiff({ head: null, cwd: tempGitDir })) === 0) {
-  //   console.log('❎ Skipping push to AlgoliaWeb because there is no change.');
+  if ((await getNbGitDiff({ head: null, cwd: tempGitDir })) === 0) {
+    console.log('❎ Skipping push to AlgoliaWeb because there is no change.');
 
-  //   return;
-  // }
+    return;
+  }
 
-  // await configureGitHubAuthor(tempGitDir);
+  await configureGitHubAuthor(tempGitDir);
 
-  // await run('git add .', { cwd: tempGitDir });
-  // await gitCommit({
-  //   message: config.commitMessage,
-  //   coAuthors: [author, ...coAuthors],
-  //   cwd: tempGitDir,
-  // });
-  // await run(`git push -f -u origin ${targetBranch}`, { cwd: tempGitDir });
+  await run('git add .', { cwd: tempGitDir });
+  await gitCommit({
+    message: config.commitMessage,
+    coAuthors: [author, ...coAuthors],
+    cwd: tempGitDir,
+  });
+  await run(`git push -f -u origin ${targetBranch}`, { cwd: tempGitDir });
 
-  // console.log(`Creating pull request on ${OWNER}/${repository}...`);
-  // const octokit = getOctokit();
-  // const { data } = await octokit.pulls.create({
-  //   owner: OWNER,
-  //   repo: repository,
-  //   title: config.commitMessage,
-  //   body: [
-  //     'This PR is automatically created by https://github.com/algolia/api-clients-automation',
-  //     'It contains the latest generated guides.',
-  //   ].join('\n\n'),
-  //   base: 'develop',
-  //   head: targetBranch,
-  // });
+  console.log(`Creating pull request on ${OWNER}/${repository}...`);
+  const octokit = getOctokit();
+  const { data } = await octokit.pulls.create({
+    owner: OWNER,
+    repo: repository,
+    title: config.commitMessage,
+    body: [
+      'This PR is automatically created by https://github.com/algolia/api-clients-automation',
+      'It contains the latest generated guides.',
+    ].join('\n\n'),
+    base: 'develop',
+    head: targetBranch,
+  });
 
-  // console.log(`Pull request created on ${OWNER}/${repository}`);
-  // console.log(`  > ${data.url}`);
+  console.log(`Pull request created on ${OWNER}/${repository}`);
+  console.log(`  > ${data.url}`);
 }
 
 if (import.meta.url.endsWith(process.argv[1])) {
