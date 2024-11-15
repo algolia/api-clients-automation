750d282e4a95c5ed019c6d47fa5e772beacc6446
Pierre Millot <millotpierre3@gmail.com>
2024-11-15 17:01:16 +0100

    fix(cts): add e2e tests for composition api [skip-bc] (#4111)
    ## 🧭 What and Why
    
    🎟 JIRA Ticket: [DI-3209](https://algolia.atlassian.net/browse/DI-3209)
    
    Setup e2e with a special metis app, granted by the fleet team only to use for this CI.

--- playground/javascript/node/composition.ts
+++ playground/javascript/node/composition.ts
@@ -1,17 +1,44 @@
 import { ApiError } from '@algolia/client-common';
 import { compositionClient } from '@algolia/client-composition';
 
-const appId = process.env.ALGOLIA_APPLICATION_ID || '**** APP_ID *****';
-const apiKey = process.env.ALGOLIA_ADMIN_KEY || '**** ADMIN_KEY *****';
+const appId = process.env.METIS_APPLICATION_ID || '**** APP_ID *****';
+const apiKey = process.env.METIS_API_KEY || '**** ADMIN_KEY *****';
 
 // Init client with appId and apiKey
 const client = compositionClient(appId, apiKey);
 
 async function testComposition() {
   try {
-    const res = await client.listCompositions();
-
+    // create a new composition
+    /*
+    const res = await client.multipleBatch({
+      requests: [
+        {
+          action: 'upsert',
+          body: {
+            objectID: 'id1',
+            description: 'the first ever composition from the client',
+            behavior: {
+              injection: {
+                main: {
+                  source: {
+                    search: {
+                      index: 'cts_e2e_small',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      ],
+    });
     console.log(`[OK]`, res);
+    */
+
+    const compos = await client.listCompositions();
+
+    console.log(`[OK]`, compos);
   } catch (e) {
     if (e instanceof ApiError) {
       return console.log(`[${e.status}] ${e.message}`, e.stackTrace, e);
