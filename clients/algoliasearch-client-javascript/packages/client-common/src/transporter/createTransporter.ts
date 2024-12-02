1389351817d16f1031fd56d0084ba06c0cd8405e
shortcuts <vannicattec@gmail.com>
2024-12-02 10:09:46 +0100

    fix: s'echarpe

--- clients/algoliasearch-client-javascript/packages/client-common/src/transporter/createTransporter.ts
+++ clients/algoliasearch-client-javascript/packages/client-common/src/transporter/createTransporter.ts
@@ -142,6 +142,8 @@ export function createTransporter({
         responseTimeout: getTimeout(timeoutsCount, isRead ? timeout.read : timeout.write),
       };
 
+      console.log(payload);
+
       /**
        * The stackFrame is pushed to the stackTrace so we
        * can have information about onRetry and onFailure
