17c1696c4895feb0e50f43aeb09fdf426a237233
shortcuts <vannicattec@gmail.com>
2024-12-17 10:51:35 +0100

    chore: update metis app for e2e

--- playground/javascript/node/composition.ts
+++ playground/javascript/node/composition.ts
@@ -9,37 +9,35 @@ const client = compositionClient(appId, apiKey);
 
 async function testComposition() {
   try {
+    console.log(appId, apiKey)
     // create a new composition
-    /*
-    const res = await client.multipleBatch({
-      requests: [
-        {
-          action: 'upsert',
-          body: {
-            objectID: 'id1',
-            name: 'my first composition',
-            description: 'the first ever composition from the client',
-            behavior: {
-              injection: {
-                main: {
-                  source: {
-                    search: {
-                      index: 'cts_e2e_small',
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      ],
-    });
-    console.log(`[OK]`, res);
-    */
+    // const res = await client.multipleBatch({
+    //   requests: [
+    //     {
+    //       action: 'upsert',
+    //       body: {
+    //         objectID: 'id1',
+    //         name: 'my first composition',
+    //         description: 'the first ever composition from the client',
+    //         behavior: {
+    //           injection: {
+    //             main: {
+    //               source: {
+    //                 search: {
+    //                   index: 'cts_e2e_small',
+    //                 },
+    //               },
+    //             },
+    //           },
+    //         },
+    //       },
+    //     },
+    //   ],
+    // });
+    // console.log(`[OK]`, res);
 
-    const compos = await client.listCompositions();
+    console.log(await client.listCompositions())
 
-    console.log(`[OK]`, compos);
   } catch (e) {
     if (e instanceof ApiError) {
       return console.log(`[${e.status}] ${e.message}`, e.stackTrace, e);
