d14b2a72a195f46552f23e19ec34eadd2bc0982d
Pierre Millot <pierre.millot@algolia.com>
2025-01-06 12:44:19 +0100

    fix(ruby): use discriminators for oneOf

--- playground/python/app/recommend.py
+++ playground/python/app/recommend.py
@@ -1,19 +1,45 @@
 from asyncio import run
+from os import environ
 
 from algoliasearch.recommend import __version__
 from algoliasearch.recommend.client import RecommendClient
+from algoliasearch.recommend.models.get_recommendations_params import (
+    GetRecommendationsParams,
+)
+from algoliasearch.recommend.models.related_query import RelatedQuery
+from algoliasearch.recommend.models.recommendations_request import (
+    RecommendationsRequest,
+)
+from algoliasearch.recommend.models.related_model import RelatedModel
+
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
 
 
 async def main():
     print("RecommendClient version", __version__)
 
-    client = RecommendClient("FOO", "BAR")
+    client = RecommendClient(
+        environ.get("ALGOLIA_APPLICATION_ID"), environ.get("ALGOLIA_ADMIN_KEY")
+    )
 
     print("client initialized", client)
 
     try:
-        response = await client.delete_recommend_rule(
-            index_name="nvim", model="test", object_id="objID"
+        response = await client.get_recommendations(
+            GetRecommendationsParams(
+                requests=[
+                    RecommendationsRequest(
+                        RelatedQuery(
+                            index_name="cts_e2e_browse",
+                            object_id="Batman Dracula",
+                            model=RelatedModel.RELATED_PRODUCTS,
+                            threshold=2,
+                        )
+                    )
+                ]
+            )
         )
 
         print(response)
