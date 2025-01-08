5aa57630d6e26ca926c7bcb51d28f9501beefc66
Pierre Millot <pierre.millot@algolia.com>
2025-01-08 16:56:26 +0100

    feat(specs): only use shortname in the source input [skip-bc] (#4287)

--- scripts/docker/Dockerfile.swift
+++ scripts/docker/Dockerfile.swift
@@ -1,15 +1,15 @@
 ARG SWIFT_VERSION
 ARG SWIFTFORMAT_VERSION=0.54.5
 
-FROM ghcr.io/nicklockwood/swiftformat:${SWIFTFORMAT_VERSION} AS swiftFormat
+FROM ghcr.io/nicklockwood/swiftformat:${SWIFTFORMAT_VERSION} AS swift_format
 FROM swift:${SWIFT_VERSION}-jammy
 
-COPY --from=swiftFormat /usr/bin/swiftformat /usr/bin/swiftformat
+COPY --from=swift_format /usr/bin/swiftformat /usr/bin/swiftformat
 
 # Global dependencies
 RUN apt-get update \
-    && apt-get install -y --no-install-recommends zlib1g-dev \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
+  && apt-get install -y --no-install-recommends zlib1g-dev \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
