01da70f9af0b374675320a1cb749cfcf791e0745
shortcuts <vannicattec@gmail.com>
2025-01-22 14:01:02 +0100

    chore: burnout

--- clients/algoliasearch-client-dart/packages/client_composition/example/example.dart
+++ clients/algoliasearch-client-dart/packages/client_composition/example/example.dart
@@ -5,7 +5,6 @@ void main() async {
   final composition = CompositionClient(
     appId: 'latency',
     apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-    options: ClientOptions(requester: requester),
   );
 
   final compositions = await composition.search(
