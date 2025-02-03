690917128bb84bf617bc271b7e0f60b6724bd351
shortcuts <vannicattec@gmail.com>
2025-02-03 21:37:11 +0100

    chore: use client test

--- scripts/cts/testServer/algoliaMock.ts
+++ scripts/cts/testServer/algoliaMock.ts
@@ -17,6 +17,59 @@ function addRoutes(app: Express): void {
       status: 'published',
     });
   });
+  app.get('/1/indexes/indexName/query', (req, res) => {
+    res.json({
+      nbHits: 1,
+      hits: [
+        {
+          _snippetResult: {
+            genres: [
+              {
+                value: 'Animated',
+                matchLevel: 'none',
+              },
+              {
+                value: 'Superhero',
+                matchLevel: 'none',
+              },
+              {
+                value: 'Romance',
+                matchLevel: 'none',
+              },
+            ],
+            year: {
+              value: '1993',
+              matchLevel: 'none',
+            },
+          },
+          _highlightResult: {
+            genres: [
+              {
+                value: 'Animated',
+                matchLevel: 'none',
+                matchedWords: [],
+              },
+              {
+                value: 'Superhero',
+                matchLevel: 'none',
+                matchedWords: [],
+              },
+              {
+                value: 'Romance',
+                matchLevel: 'none',
+                matchedWords: [],
+              },
+            ],
+            year: {
+              value: '1993',
+              matchLevel: 'none',
+              matchedWords: [],
+            },
+          },
+        },
+      ],
+    });
+  });
 }
 
 export function algoliaMockServer(): Promise<Server> {
