44621dbd3701e156aae44e0bfc1b8dedd6b752e6
Thomas Raffray <Fluf22@users.noreply.github.com>
2025-02-21 20:27:28 +0100

    fix: scala playground 2

--- playground/scala/src/main/scala/search.scala
+++ playground/scala/src/main/scala/search.scala
@@ -7,41 +7,43 @@ import org.json4s.jvalue2extractable
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContextExecutor}
 
-def main(): Unit = {
-  implicit val ec: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
-  implicit val formats: org.json4s.Formats = org.json4s.DefaultFormats
+class Main {
+  case class Actor(name: String)
+  def main(args: Array[String]): Unit = {
+    implicit val ec: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
+      implicit val formats: org.json4s.Formats = org.json4s.DefaultFormats
 
-  val dotenv = Dotenv.configure.directory("../").load
-  val appId = dotenv.get("ALGOLIA_APPLICATION_ID")
-  val apiKey = dotenv.get("ALGOLIA_ADMIN_KEY")
-  val indexName = dotenv.get("SEARCH_INDEX")
-  val query = dotenv.get("SEARCH_QUERY")
+      val dotenv = Dotenv.configure.directory("../").load
+      val appId = dotenv.get("ALGOLIA_APPLICATION_ID")
+      val apiKey = dotenv.get("ALGOLIA_ADMIN_KEY")
+      val indexName = dotenv.get("SEARCH_INDEX")
+      val query = dotenv.get("SEARCH_QUERY")
 
-  val client = SearchClient(
-    appId = appId,
-    apiKey = apiKey,
-    clientOptions = ClientOptions
-      .builder()
-      .withLogging(Logging.Full)
-      .withAgentSegment("playground")
-      .build()
-  )
-
-  val params = SearchMethodParams(
-    requests = Seq(
-      SearchForHits(
-        indexName = indexName,
-        query = Some(query)
+      val client = SearchClient(
+        appId = appId,
+        apiKey = apiKey,
+        clientOptions = ClientOptions
+          .builder()
+          .withLogging(Logging.Full)
+          .withAgentSegment("playground")
+          .build()
       )
-    )
-  )
-  val res = client.search(searchMethodParams = params)
-  val value = Await.result(res, Duration(100, "sec"))
-  val response = value.results.head.asInstanceOf[SearchResponse]
-  for (hit <- response.hits) {
-    //val actor = hit.extract[Actor]
-    //println(actor)
+
+      val params = SearchMethodParams(
+        requests = Seq(
+          SearchForHits(
+            indexName = indexName,
+            query = Some(query)
+          )
+        )
+      )
+      val res = client.search(searchMethodParams = params)
+      val value = Await.result(res, Duration(100, "sec"))
+      val response = value.results.head.asInstanceOf[SearchResponse]
+      for (hit <- response.hits) {
+        //val actor = hit.extract[Actor]
+        //println(actor)
+      }
   }
 }
 
-case class Actor(name: String)
