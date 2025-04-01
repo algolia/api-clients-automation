bbdc14e5f92f4241ae9ddbe2227affca6459673d
Pierre Millot <pierre.millot@algolia.com>
2025-04-01 17:06:16 +0200

    chore(scripts): reuse java-version

--- scripts/ci/githubActions/utils.ts
+++ scripts/ci/githubActions/utils.ts
@@ -70,7 +70,7 @@ function getVersionFileForLanguage(lang: Language): string {
 
   // jvm lang rely on the same java version
   if (lang === 'kotlin' || lang === 'java' || lang === 'scala') {
-    return 'config/.java-version';
+    return 'clients/algoliasearch-client-java/.java-version';
   }
 
   return `config/.${lang}-version`;
