8e100703f8833a3141eab86c7d76a362cf69cfd0
shortcuts <vannicattec@gmail.com>
2025-04-28 09:15:48 +0200

    commands

--- scripts/formatter.ts
+++ scripts/formatter.ts
@@ -6,7 +6,7 @@ export async function formatter(language: string, cwd: string): Promise<void> {
   switch (language) {
     case 'csharp':
       if (cwd.includes('tests') || cwd.includes('snippets') || cwd.includes('guides')) {
-        await run('dotnet format --no-restore && dotnet tool restore && dotnet dotnet-csharpier .', {
+        await run('dotnet format --no-restore && dotnet tool restore && dotnet csharpier .', {
           cwd,
           language,
         });
