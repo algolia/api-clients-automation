e9ff3938d9e1b8d351a9403efe1c5fe9eac63bf1
shortcuts <vannicattec@gmail.com>
2025-06-06 10:56:08 +0200

    chore: idk

--- scripts/ci/codegen/pushToRepository.ts
+++ scripts/ci/codegen/pushToRepository.ts
@@ -4,7 +4,6 @@ import path, { resolve } from 'path';
 import {
   CLIENTS,
   configureGitHubAuthor,
-  ensureGitHubToken,
   exists,
   getOctokit,
   gitBranchExists,
@@ -114,8 +113,6 @@ async function handleGuideFiles(guide: GuidesToPush, tempGitDir: string): Promis
 }
 
 async function pushToRepository(repository: string, config: RepositoryConfiguration): Promise<void> {
-  const token = ensureGitHubToken();
-
   const lastCommitMessage = await run('git log -1 --format="%s"');
   const author = (await run('git log -1 --format="Co-authored-by: %an <%ae>"')).trim();
   const coAuthors = (await run('git log -1 --format="%(trailers:key=Co-authored-by)"'))
@@ -143,7 +140,7 @@ async function pushToRepository(repository: string, config: RepositoryConfigurat
 
   await configureGitHubAuthor(tempGitDir);
 
-  await run(`git remote set-url origin https://${token}@github.com/${OWNER}/${repository}.git`);
+  await run(`git remote set-url origin git@github.com:${OWNER}/${repository}.git`);
 
   for (const task of config.tasks) {
     console.log(`Handling '${task.files.type}' file(s)`);
