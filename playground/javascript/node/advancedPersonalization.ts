bb9bfe012e848b04784400b360a9416ca638eef7
Raed <raed.chammam@algolia.com>
2025-06-13 14:34:07 +0200

    add to playground

---
+++ playground/javascript/node/advancedPersonalization.ts
@@ -0,0 +1,24 @@
+import { advancedPersonalizationClient } from '@algolia/client-advanced-personalization';
+import { ApiError } from '@algolia/client-common';
+
+const appId = process.env.ALGOLIA_APPLICATION_ID || '**** APP_ID *****';
+const apiKey = process.env.ALGOLIA_RECOMMENDATION_KEY || '**** RECOMMENDATION_API_KEY *****';
+
+// Init client with appId and apiKey
+const client = advancedPersonalizationClient(appId, apiKey, 'eu');
+
+async function testPersonalization() {
+  try {
+    const res = await client.computeRealtimeUser({ userToken: 'userToken' });
+
+    console.log(`[OK]`, res);
+  } catch (e) {
+    if (e instanceof ApiError) {
+      return console.log(`[${e.status}] ${e.message}`, e.stackTrace);
+    }
+
+    console.log('[ERROR]', e);
+  }
+}
+
+testPersonalization();
