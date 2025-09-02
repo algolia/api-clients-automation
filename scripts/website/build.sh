007a2995d08e6b9006f0d8e1b94aa9ce89ac494f
Thomas Raffray <Fluf22@users.noreply.github.com>
2025-09-02 17:15:18 +0200

    chore(website): exclude schema from generated variables file

--- scripts/website/build.sh
+++ scripts/website/build.sh
@@ -4,7 +4,7 @@
 touch website/yarn.lock
 
 # Generate the file with constants
-cat config/clients.config.json | jq -r '"export const versions = \(map_values(.packageVersion))"' > website/src/generated/variables.js
+cat config/clients.config.json | jq -r 'del(."$schema") | "export const versions = \(map_values(.packageVersion))"' > website/src/generated/variables.js
 
 # install website deps and build
 cd website && yarn install && yarn build
