f27d7aa56dbbb4f871e1b39743fd12844999576f
Pierre Millot <pierre.millot@algolia.com>
2025-11-26 11:30:36 +0100

    chore(scripts): update the jira action

---
+++ scripts/update_jira_token.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# This script updates the JIRA_TOKEN secret in the GitHub repository
+# Usage: ./scripts/update_jira_token.sh <jira_email> <new_jira_token>
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <jira_email> <new_jira_token>"
+    exit 1
+fi
+
+EMAIL="$1"
+TOKEN="$2"
+ENCRYPED_TOKEN=$(echo -n "$EMAIL:$TOKEN" | base64)
+
+repositories=(
+  "algolia/api-clients-automation"
+  "algolia/algoliasearch-client-csharp"
+  "algolia/algoliasearch-client-dart"
+  "algolia/algoliasearch-client-go"
+  "algolia/algoliasearch-client-java"
+  "algolia/algoliasearch-client-javascript"
+  "algolia/algoliasearch-client-kotlin"
+  "algolia/algoliasearch-client-php"
+  "algolia/algoliasearch-client-python"
+  "algolia/algoliasearch-client-ruby"
+  "algolia/algoliasearch-client-scala"
+  "algolia/algoliasearch-client-swift"
+)
+
+for repo in "${repositories[@]}"; do
+  gh -R "$repo" secret set JIRA_TOKEN --body "$ENCRYPED_TOKEN"
+done
