f9d12f154f10aee5a8c118d2864ce7a2636c97a3
Thomas Raffray <Fluf22@users.noreply.github.com>
2026-02-25 21:57:17 +0100

    fix: agent-studio client build

---
+++ playground/java/src/main/java/com/algolia/playground/AgentStudio.java
@@ -0,0 +1,25 @@
+package com.algolia.playground;
+
+import com.algolia.api.AgentStudioClient;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class AgentStudio {
+
+  public static void main(String[] args) throws Exception {
+    var dotenv = Dotenv.configure().directory("../").load();
+    var appId = dotenv.get("ALGOLIA_APPLICATION_ID");
+    var apiKey = dotenv.get("ALGOLIA_ADMIN_KEY");
+
+    var client = new AgentStudioClient(appId, apiKey);
+
+    try {
+      var response = client.listAgents();
+      System.out.println("List of agents:");
+      for (var agent : response.getAgents()) {
+        System.out.println("- " + agent.getName() + " (ID: " + agent.getId() + ")");
+      }
+    } finally {
+      client.close();
+    }
+  }
+}
