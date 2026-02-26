13b19b38f1939d3c229cdd4e9a16e680a3286063
Thomas Raffray <Fluf22@users.noreply.github.com>
2026-02-26 22:26:52 +0100

    feat: add region parameter to AgentStudioClient initialization and implement agent studio playground

--- playground/ruby/agent-studio.rb
+++ playground/ruby/agent-studio.rb
@@ -3,7 +3,7 @@ require 'algolia'
 
 Dotenv.load('../.env')
 
-client = Algolia::AgentStudioClient.create(ENV['ALGOLIA_APPLICATION_ID'], ENV['ALGOLIA_ADMIN_KEY'])
+client = Algolia::AgentStudioClient.create(ENV['ALGOLIA_APPLICATION_ID'], ENV['ALGOLIA_ADMIN_KEY'], 'us')
 
 response = client.list_agents
 puts 'List of agents:'
