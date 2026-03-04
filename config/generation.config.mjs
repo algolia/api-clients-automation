ee30a75b0ff986935191fed1d9d2cf29321dc081
Thomas Raffray <Fluf22@users.noreply.github.com>
2026-03-04 08:18:57 +0100

    feat: implement lenient JSON assertion helper and update HTTP request handling

--- config/generation.config.mjs
+++ config/generation.config.mjs
@@ -63,6 +63,7 @@ export const patterns = [
   'clients/algoliasearch-client-java/algoliasearch/src/main/java/com/algolia/BuildConfig.java',
 
   'tests/output/java/build.gradle',
+  '!tests/output/java/src/test/java/com/algolia/utils/**',
 
   // JavaScript
   '!clients/algoliasearch-client-javascript/*',
@@ -90,6 +91,8 @@ export const patterns = [
   'clients/algoliasearch-client-kotlin/client/src/commonMain/kotlin/com/algolia/client/BuildConfig.kt',
   'clients/algoliasearch-client-kotlin/client/src/commonMain/kotlin/com/algolia/client/api/**',
   'clients/algoliasearch-client-kotlin/client/src/commonMain/kotlin/com/algolia/client/model/**',
+
+  '!tests/output/kotlin/src/commonTest/kotlin/com/algolia/utils/TestHelpers.kt',
 
   // PHP
   '!clients/algoliasearch-client-php/**',
