299f1a4fe8cff16e4f2785b112c58641ec10f6a7
mario.danmarioalexandrudan <marioalexandrudan@gmail.com>
2026-03-10 17:23:24 +0200

    feat(dart): implement gzip compression for string data using archive package

--- clients/algoliasearch-client-dart/packages/client_core/lib/src/transport/dio/compress_web.dart
+++ clients/algoliasearch-client-dart/packages/client_core/lib/src/transport/dio/compress_web.dart
@@ -1,1 +1,7 @@
-List<int>? gzipCompress(String data) => null;
+import 'dart:convert';
+
+import 'package:archive/archive.dart';
+
+List<int>? gzipCompress(String data) {
+  return GZipEncoder().encode(utf8.encode(data));
+}
