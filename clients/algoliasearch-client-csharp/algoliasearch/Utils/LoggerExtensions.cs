4d887a7df78362188ed4bd5722539ce9af009d15
mario.danmarioalexandrudan <marioalexandrudan@gmail.com>
2026-03-10 15:48:37 +0200

    chore(csharp): remove unused LoggerExtensions

--- clients/algoliasearch-client-csharp/algoliasearch/Utils/LoggerExtensions.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.Logging;
-
-namespace Algolia.Search.Utils;
-
-internal static class LoggerExtensions
-{
-  public static void LogInfo(this ILogger logger, string message, params object[] args) =>
-    logger.LogInformation(message, args);
-}
