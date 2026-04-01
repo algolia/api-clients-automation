32a056e72e6689ed5f76c04e1cf27ed62fa82728
eric-zaharia <zahariaeric@gmail.com>
2026-04-01 16:48:25 +0300

    fix(php): remove filter

--- clients/algoliasearch-client-php/lib/RequestOptions/RequestOptionsFactory.php
+++ clients/algoliasearch-client-php/lib/RequestOptions/RequestOptionsFactory.php
@@ -26,14 +26,14 @@ final class RequestOptionsFactory
 
             $options = new RequestOptions($options);
         } elseif ($options instanceof RequestOptions) {
-            $options = $this->create(array_filter([
+            $options = $this->create([
                 'headers' => $options->getHeaders(),
                 'queryParameters' => $options->getQueryParameters(),
                 'body' => $options->getBody(),
                 'readTimeout' => $options->getReadTimeout(),
                 'writeTimeout' => $options->getWriteTimeout(),
                 'connectTimeout' => $options->getConnectTimeout(),
-            ], fn ($v) => null !== $v && [] !== $v));
+            ]);
         } else {
             throw new \InvalidArgumentException('RequestOptions can only be created from array or from RequestOptions object');
         }
