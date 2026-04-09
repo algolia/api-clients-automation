d57615cf3c9b1c21b1e310dcb3805fe92e2e706e
Clara Muller <5667350+ClaraMuller@users.noreply.github.com>
2026-04-09 17:09:02 +0200

    test: update manual tests

--- tests/output/go/tests/manual/composition_oneof_test.go
+++ tests/output/go/tests/manual/composition_oneof_test.go
@@ -19,9 +19,9 @@ func TestInjectedItemSource_UnmarshalSearchSource(t *testing.T) {
 	err := json.Unmarshal([]byte(input), &source)
 	require.NoError(t, err)
 
-	require.NotNil(t, source.SearchSource, "expected SearchSource to be set")
+	require.NotNil(t, source.InjectedItemSourceSearchSource, "expected InjectedItemSourceSearchSource to be set")
 	require.Nil(t, source.ExternalSource, "expected ExternalSource to be nil")
-	require.Equal(t, "demo", source.SearchSource.Search.Index)
+	require.Equal(t, "demo", source.InjectedItemSourceSearchSource.Search.Index)
 }
 
 func TestInjectedItemSource_UnmarshalExternalSource(t *testing.T) {
@@ -35,7 +35,7 @@ func TestInjectedItemSource_UnmarshalExternalSource(t *testing.T) {
 	require.NoError(t, err)
 
 	require.NotNil(t, source.ExternalSource, "expected ExternalSource to be set")
-	require.Nil(t, source.SearchSource, "expected SearchSource to be nil")
+	require.Nil(t, source.InjectedItemSourceSearchSource, "expected InjectedItemSourceSearchSource to be nil")
 	require.Equal(t, "sponsored", source.ExternalSource.External.Index)
 }
 
@@ -60,6 +60,36 @@ func TestInjectedItemSource_RoundTripExternalSource(t *testing.T) {
 	input := `{"external":{"index":"sponsored"}}`
 
 	var source composition.InjectedItemSource
+
+	err := json.Unmarshal([]byte(input), &source)
+	require.NoError(t, err)
+
+	output, err := json.Marshal(&source)
+	require.NoError(t, err)
+	require.JSONEq(t, input, string(output))
+}
+
+func TestInjectionMainSource_UnmarshalSearchSource(t *testing.T) {
+	t.Parallel()
+
+	input := `{"search": {"index": "demo"}}`
+
+	var source composition.InjectionMainSource
+
+	err := json.Unmarshal([]byte(input), &source)
+	require.NoError(t, err)
+
+	require.NotNil(t, source.SearchSource, "expected SearchSource to be set")
+	require.Nil(t, source.RecommendSource, "expected RecommendSource to be nil")
+	require.Equal(t, "demo", source.SearchSource.Search.Index)
+}
+
+func TestInjectionMainSource_RoundTripSearchSource(t *testing.T) {
+	t.Parallel()
+
+	input := `{"search":{"index":"demo"}}`
+
+	var source composition.InjectionMainSource
 
 	err := json.Unmarshal([]byte(input), &source)
 	require.NoError(t, err)
