c07ff97d49177e4ac61aa28b08b204b1a9b509d6
mario.danmarioalexandrudan <marioalexandrudan@gmail.com>
2026-07-13 15:36:54 +0300

    fix(swift): reduce per-request client overhead

--- clients/algoliasearch-client-swift/Sources/Core/AnyCodable/AnyDecodable.swift
+++ clients/algoliasearch-client-swift/Sources/Core/AnyCodable/AnyDecodable.swift
@@ -61,10 +61,10 @@ extension _AnyDecodable {
             #endif
         } else if let string = try? container.decode(String.self) {
             self.init(string)
-        } else if let bool = try? container.decode(Bool.self) {
-            self.init(bool)
         } else if let int = try? container.decode(Int.self) {
             self.init(int)
+        } else if let bool = try? container.decode(Bool.self) {
+            self.init(bool)
         } else if let uint = try? container.decode(UInt.self) {
             self.init(uint)
         } else if let double = try? container.decode(Double.self) {
