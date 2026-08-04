58737f3c20b263d2c46f614a576fb55d0b42f5e5
Eric Zaharia <94015633+eric-zaharia@users.noreply.github.com>
2026-08-04 10:11:07 +0300

    feat(cts): Request-ID/Correlation-ID CTS foundation (#6754)

--- generators/src/main/java/com/algolia/codegen/cts/tests/ResponseProp.java
+++ generators/src/main/java/com/algolia/codegen/cts/tests/ResponseProp.java
@@ -9,12 +9,15 @@ public class ResponseProp {
   @JsonDeserialize(using = RawDeserializer.class)
   public String body;
 
+  public String correlationIdSuffix;
+
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class ResponseProp {\n");
     sb.append("    statusCode: ").append(statusCode).append("\n");
     sb.append("    body: ").append(body).append("\n");
+    sb.append("    correlationIdSuffix: ").append(correlationIdSuffix).append("\n");
     sb.append("}");
     return sb.toString();
   }
