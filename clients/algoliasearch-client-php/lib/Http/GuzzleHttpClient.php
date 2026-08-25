917c94eb1f4703b3fe73c304efcdc3d0c3f4b060
Eric Zaharia <94015633+eric-zaharia@users.noreply.github.com>
2026-08-25 17:18:34 +0300

    chore(php): cover the guzzle transport in CI (#6948)

--- clients/algoliasearch-client-php/lib/Http/GuzzleHttpClient.php
+++ clients/algoliasearch-client-php/lib/Http/GuzzleHttpClient.php
@@ -5,7 +5,9 @@ namespace Algolia\AlgoliaSearch\Http;
 use Algolia\AlgoliaSearch\Exceptions\TimeoutException;
 use Algolia\AlgoliaSearch\Http\Psr7\Response;
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ConnectTimeoutException;
 use GuzzleHttp\Exception\HandlerClosedException;
+use GuzzleHttp\Exception\NetworkTimeoutException;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\ResponseTimeoutException;
 use GuzzleHttp\HandlerStack;
@@ -32,9 +34,10 @@ final class GuzzleHttpClient implements HttpClientInterface
             $response = $this->client->send($request, [
                 'timeout' => $timeout,
                 'connect_timeout' => $connectTimeout,
+                'decode_content' => 'gzip',
             ]);
         } catch (HandlerClosedException|NetworkExceptionInterface|ResponseTimeoutException $e) {
-            throw new TimeoutException($e->getMessage(), 0, $e);
+            throw new TimeoutException(self::isTimeout($e) ? 'Connection timed out' : $e->getMessage(), 0, $e);
         } catch (RequestException $e) {
             $response = method_exists($e, 'getResponse') ? $e->getResponse() : null;
             if (null !== $response) {
@@ -45,6 +48,18 @@ final class GuzzleHttpClient implements HttpClientInterface
         }
 
         return $response;
+    }
+
+    private static function isTimeout(\Throwable $e): bool
+    {
+        if ($e instanceof ResponseTimeoutException
+            || $e instanceof ConnectTimeoutException
+            || $e instanceof NetworkTimeoutException) {
+            return true;
+        }
+
+        return method_exists($e, 'getHandlerContext')
+            && CURLE_OPERATION_TIMEDOUT === ($e->getHandlerContext()['errno'] ?? 0);
     }
 
     private static function buildClient(array $config = [])
